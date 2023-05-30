4963b0c94f82c94165eeecb21a24ee8ae1a0fc56
Ethan Krimer <eitan.krimer@gmail.com>
2023-05-30 10:32:26 +0300

    added basic routing for login and register

--- cv_next/app/page.tsx
+++ cv_next/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World!</h1>
+      <Link href={"/pages/login"}>Log In</Link>
+      <Link href={"/pages/register"}>Register</Link>
     </main>
   )
 }
---
+++ cv_next/app/pages/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Register</h1>
+    </main>
+  )
+}
