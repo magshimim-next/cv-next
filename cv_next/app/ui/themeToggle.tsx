4dc63e15945480022bd4951922866fae63059564
avnogy <avner.mindelis@gmail.com>
2023-06-06 15:22:20 +0300

    created theme toggle button

---
+++ cv_next/app/ui/themeToggle.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons-react';
+
+export default function SwitchToggle() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'blue'}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+    </ActionIcon>
+  );
+}
