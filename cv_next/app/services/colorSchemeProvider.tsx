9a61cdb3d6026d0fdca204c6ac5112648d88b9e1
avnogy <avner.mindelis@gmail.com>
2023-06-06 15:19:50 +0300

    added mantine colorscheme provider

---
+++ cv_next/app/services/colorSchemeProvider.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+
+export default function ColorProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "dark",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+    
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}
+      >
+        {children}
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
