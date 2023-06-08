046a6eecd3902d19a067378af415fd90ba874eb6
avnogy <avner.mindelis@gmail.com>
2023-06-08 12:58:23 +0300

    added feed layout and base components

---
+++ cv_next/app/pages/feed/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/app/ui/feed/feedLayout";
+export default function Home() {
+  return (
+    <main>
+      <Layout>
+        <h1>Page Content</h1>
+      </Layout>
+    </main>
+  );
+}
---
+++ cv_next/app/ui/feed/feedHeader.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@mantine/core";
+import SwitchToggle from "../themeToggle";
+function FeedNavbar() {
+  return (
+    <div>
+      <Text>Application header</Text>
+      <SwitchToggle/>
+    </div>
+  );
+}
+
+export default FeedNavbar;
