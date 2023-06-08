28cbff41b2f00bf4f0580b74f86d0c1e4b0ddb5d
avnogy <avner.mindelis@gmail.com>
2023-06-09 01:03:23 +0300

    fixed scrolling (hopefully)

--- cv_next/app/ui/feed/feedItems.tsx
+++ cv_next/app/ui/feed/feedItems.tsx
@@ -1,72 +1,72 @@
 "use client";
 import CvModel from "@/app/models/cv";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { useIntersection } from "@mantine/hooks";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import FirebaseHelper from "@/app/services/firebaseHelper";
+// import CVCard from "./cvCard";
 
-const CVS_PER_PAGE = 2;
-
-const mockcvs: CvModel[] = [
-  new CvModel("1", "1", "https://site.com/1", 1),
-  new CvModel("2", "2", "https://site.com/2", 1),
-  new CvModel("3", "3", "https://site.com/3", 1),
-  new CvModel("4", "4", "https://site.com/4", 1),
-  new CvModel("5", "5", "https://site.com/5", 1),
-  new CvModel("6", "6", "https://site.com/6", 1),
-  new CvModel("7", "7", "https://site.com/7", 1),
-  new CvModel("8", "8", "https://site.com/8", 1),
-];
-
-// mock database fetch
-const fetchCVs = async (page: number) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return mockcvs.slice((page - 1) * CVS_PER_PAGE, page * CVS_PER_PAGE);
+//TESTING ONLY
+//hook for generating different ids in mock models
+const useIncrementState = (initialValue: number) => {
+  const [value, setValue] = useState<number>(initialValue);
+  const setValueWithIncrement = (newValue: number) => {
+    setValue(newValue + 1);
+  };
+  return [value, setValueWithIncrement] as const;
 };
+//TESTING ONLY
 
 function FeedItems() {
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
-    ["query"],
-    async ({ pageParam = 1 }) => {
-      const response = await fetchCVs(pageParam);
-      return response;
-    },
-    {
-      getNextPageParam: (_, mockcvs) => {
-        return mockcvs.length + 1;
-      },
-      initialData: {
-        pages: [mockcvs.slice(0, CVS_PER_PAGE)],
-        pageParams: [1],
-      },
-    }
-  );
+  const [cvs, setCvs] = useState<CvModel[]>([
+    new CvModel("1", "1", "https://site.com/1", 1), //TESTING ONLY
+  ]);
 
-  const lastPostRef = useRef<HTMLElement>(null);
+  const lastCVRef = useRef<any>(null);
   const { ref, entry } = useIntersection({
-    root: lastPostRef.current,
+    root: lastCVRef.current,
     threshold: 0, //% of the viewd element before the next fetch
   });
 
+  const fetchCVs = async () => {
+    // let result = await FirebaseHelper.getAllCvs(true);
+    let result = mockFetch(); //TESTING ONLY
+    if (result !== null) {
+      setCvs(cvs.concat(result));
+      console.log("fetching..");
+    } else {
+      console.log("reached the end.");
+    }
+  };
+
+  //TESTING ONLY
+  const [count, setCount] = useIncrementState(2);
+  //mock fetch for testing
+  function mockFetch() {
+    setCount(count);
+    if (count < 500)
+      return [
+        new CvModel("" + count, "" + count, "https://site.com/" + count, count),
+      ];
+    return null;
+  }
+  //TESTING ONLY
+
   useEffect(() => {
-    if (entry?.isIntersecting) fetchNextPage();
+    if (entry?.isIntersecting) {
+      fetchCVs();
+    }
   }, [entry]);
 
-  const cvs = data?.pages.flatMap((page) => page);
   return (
     <div>
-      {cvs?.map((cv, i) => {
-        if (i === cvs.length - 1) {
-          return (
-            <div key={cv.id} ref={ref}>
-              {cv.documentLink}
-            </div>
-          );
-        }
-        return <div key={cv.id}>{cv.documentLink}</div>;
+      {cvs?.map((cv: CvModel, i) => {
+        return (
+          <div key={cv.id} ref={i === cvs.length - 1 ? ref : null}>
+            {/* <CVCard cv={cv} /> */}
+            {cv.id}
+          </div>
+        );
       })}
-      <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-        {isFetchingNextPage ? "Loading more... " : "Load More"}
-      </button>
     </div>
   );
 }
