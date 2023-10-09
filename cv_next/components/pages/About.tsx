9aad988f63f3d1e377a63bd9aeafaf5dfe67b35e
Nevo Sznajder <nevosznajder@gmail.com>
2023-10-09 21:26:13 +0000

    Update About.tsx

--- cv_next/components/pages/About.tsx
+++ cv_next/components/pages/About.tsx
@@ -1,19 +1,50 @@
-import "./About.css";
+import CSS from 'csstype';
+
+const About: CSS.Properties =  {
+  textAlign: 'center',
+  backgroundColor: '#3577fc',
+  color: 'white',
+  minHeight: '150vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
+const AboutHeader: CSS.Properties =  {
+  fontSize: 'calc(10px + 2vmin)'
+}
+
+const h1: CSS.Properties =  {
+  fontSize: '40px'
+}
+
+const h2: CSS.Properties =  {
+  fontSize: '30px'
+}
+
+const ul: CSS.Properties =  {
+  fontSize: '20px'
+}
+
+const p: CSS.Properties =  {
+  fontSize: '20px'
+}
 
 
 export default function AboutPage() {
   return (
-      <div className="About">
-        <header className="About-header">
+      <div className="About" style={About}>
+        <header className="About-header" style={AboutHeader}>
           <h1>About CV-Next</h1>
         </header>
         <body>
-          <h2>Designated users</h2>
-          <p>Magshimim alumni who want to <br />
+          <h2 style={h2}>Designated users</h2>
+          <p style={p}>Magshimim alumni who want to <br />
           recruit or work at other alumni.</p>
-          <h3>Work Fields</h3>
-          <p>Could be Magshimim related, and unrelated: </p>
-          <ul>
+          <h3 style={h2}>Work Fields</h3>
+          <p style={p}>Could be Magshimim related, and unrelated: </p>
+          <ul style={ul}>
             <li>Cybersecurity</li>
             <li>Software engineering</li>
             <li>Law</li>
@@ -23,8 +54,8 @@ export default function AboutPage() {
             <li>Research</li>
             <li> ... </li>
           </ul>
-          <h4>Structure</h4>
-          <p>
+          <h4 style={h2}>Structure</h4>
+          <p style={p}>
             It's similar to Twitter, in some ways. <br />
             First, login. With either a Google or <br />
             a Twitter account. Moreover, there's a <br/>
