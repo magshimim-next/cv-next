54b2f4791c2de7e7a58e50356d628ac3488643af
avnogy <avner.mindelis@gmail.com>
2023-10-13 14:51:18 +0300

    moved to next/image and fixed names according to the new cv model

--- cv_next/components/CVItem.tsx
+++ cv_next/components/CVItem.tsx
@@ -1,4 +1,6 @@
-import CVModel from "@/types/cv"
+'use client'
+import CVModel from "@/types/models/cv"
+import Image from "next/image"
 import { generateImageUrl } from "@/helpers/imageURLHelper"
 
 interface CVCardProps {
@@ -8,19 +10,22 @@ interface CVCardProps {
 export default function CVItem({ cv }: CVCardProps) {
   return (
     <div className="h-6/6 relative w-full max-w-sm rounded rounded-lg object-cover shadow-lg">
-      <img
+      <Image
+        width={500}
+        height={500 * 1.4142}
         className="w-full rounded-lg"
-        src={generateImageUrl(cv.documentUrl)}
+        src={generateImageUrl(cv.documentLink)}
         alt="CV Preview"
+        loading="lazy"
       />
       <div className="overlay gradient-blur-backdrop absolute bottom-0 left-0 flex h-1/2 w-full flex-col justify-end rounded-lg">
         <div className="px-6 pt-2">
-          <div className="text-xl font-bold text-neutral-700">{cv.userId}</div>
+          <div className="text-xl font-bold text-neutral-700">{cv.userID}</div>
           <p className="mb-2 text-base text-neutral-700">{cv.description}</p>
         </div>
         <div className="px-6 pb-2">
           <span className="mb-2 mr-2 inline-block rounded-full bg-neutral-200 px-3 py-1 text-sm font-semibold text-neutral-700">
-            #{cv.categoryId}
+            #{cv.categoryID}
           </span>
         </div>
       </div>
