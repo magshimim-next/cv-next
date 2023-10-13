3afbc4de389e7095449119d80f75958b9f6bd734
avnogy <avner.mindelis@gmail.com>
2023-10-13 15:10:28 +0300

    connected basic fetching without pagination - wip

--- cv_next/app/feed/page.tsx
+++ cv_next/app/feed/page.tsx
@@ -1,24 +1,34 @@
+"use client"
 import CVItem from "@/components/CVItem"
-import CVModel from "@/types/cv"
+import CVModel from "@/types/models/cv"
+import CvsApi from "@/server/api/cvs"
+import { useState, useEffect } from "react"
 
 export default function Feed() {
-  let mockcv = new CVModel(
-    "cv-id",
-    "user-id",
-    "https://lh3.google.com/u/0/d/1gquDwmvOjuRCVQJ-LkMnA3NOLWDw_UDu=w1000-h1200-p",
-    1,
-    "description"
-  )
+  const [cvs, setCvs] = useState<CVModel[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchedCvs = await CvsApi.getAllCvs(true)
+        if (fetchedCvs) {
+          setCvs(fetchedCvs)
+        }
+      } catch (error) {
+        console.error("Error fetching CVs:", error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <main>
       <div className="container mx-auto space-y-8 p-6">
         <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
-          <CVItem cv={mockcv} />
-          <CVItem cv={mockcv} />
-          <CVItem cv={mockcv} />
-          <CVItem cv={mockcv} />
-          <CVItem cv={mockcv} />
-          <CVItem cv={mockcv} />
+          {cvs.map((cv) => (
+            <CVItem key={cv.id} cv={cv} />
+          ))}
         </div>
       </div>
     </main>
