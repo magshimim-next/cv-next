14bb8243dc5048e5ffb47b1ce6a6264360520a21
avnogy <avner.mindelis@gmail.com>
2023-10-14 22:27:14 +0300

    replace description with date and refactored category link

--- cv_next/components/CVItem.tsx
+++ cv_next/components/CVItem.tsx
@@ -9,23 +9,30 @@ interface CVCardProps {
 }
 
 export default function CVItem({ cv }: CVCardProps) {
+  const categoryLink = `/feed?category=${Categories.category[
+    cv.categoryID
+  ].toLowerCase()}`
   return (
     <div className="h-6/6 relative w-full max-w-sm rounded rounded-lg object-cover shadow-lg">
       <Image
         width={500}
         height={500 * 1.4142}
-        className="w-full rounded-lg"
+        className="w-full rounded-lg "
         src={generateImageUrl(cv.documentLink)}
         alt="CV Preview"
         loading="lazy"
       />
       <div className="overlay gradient-blur-backdrop absolute bottom-0 flex h-1/2 w-full rounded-lg">
-        <div className=" absolute bottom-0 left-0 mx-4 mb-3">
-          <div className="text-xl font-bold text-neutral-700">{cv.userID}</div>
-          <p className="text-base text-neutral-700">{cv.description}</p>
+        <div className=" absolute bottom-0 left-0 mx-5 mb-3">
+          <div className="mr-2 inline text-xl font-bold text-neutral-700">
+            {cv.userID}
+          </div>
+          <p className="inline text-xs text-neutral-700/75">
+            {new Date(cv.uploadDate).toLocaleDateString("en-US")}
+          </p>
         </div>
         <Link
-          href={`/feed?category=${Categories.category[cv.categoryID].toLowerCase()}`}
+          href={categoryLink}
           className="absolute bottom-0 right-0 mx-4 mb-3 rounded-full bg-neutral-200 px-3 py-1 text-sm font-semibold text-neutral-700 hover:underline"
         >
           #{Categories.category[cv.categoryID]}
