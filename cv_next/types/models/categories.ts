a00b94c36da1c459f9e03bb8c1ba47b31c9b5346
avnogy <avner.mindelis@gmail.com>
2023-10-14 20:40:34 +0300

    chang categories to title case

--- cv_next/types/models/categories.ts
+++ cv_next/types/models/categories.ts
@@ -1,16 +1,16 @@
 export namespace Categories {
   export enum category {
-    undefined,
-    general,
-    medical,
-    insurance,
-    financial,
-    legal,
-    education,
-    fullStack,
-    frontEnd,
-    backEnd,
-    devOps,
-    cyberSecurity,
+    Undefined,
+    General,
+    Medical,
+    Insurance,
+    Financial,
+    Legal,
+    Education,
+    Fullstack,
+    Frontend,
+    Backend,
+    Devops,
+    Cybersecurity,
   }
 }
