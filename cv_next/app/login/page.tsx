ec33872676165b58bfc8dd03019a698c9dc1b20f
avnogy <avner.mindelis@gmail.com>
2023-12-05 00:22:11 +0200

    make google button with logo

--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -1,19 +1,39 @@
+import Image from "next/image"
+
 export default function Page() {
   return (
     <main>
       <div>
-        <h1 className="bg-blue-300 relative p-2 text-xl">
-          Select login method
-        </h1>
+        <h1 className="relative p-2 text-xl">Select login method</h1>
       </div>
-      <div className="flex flex-row gap-x-64">
+      <div className="flex flex-row">
         <div>
-          <button className="bg-blue-300 relative p-2 text-xl mr-auto">Google</button>
+          <button className="flex gap-2 rounded-lg border border-slate-200 px-4 py-2 text-slate-700 hover:border-slate-400 hover:shadow dark:border-slate-700 dark:text-slate-200 dark:hover:border-slate-500">
+            <Image
+              className="h-6 w-6"
+              src="google.svg"
+              loading="lazy"
+              alt="google logo"
+              width={6}
+              height={6}
+            />
+            <span>Login with Google</span>
+          </button>
         </div>
         <div>
-          <button className="bg-blue-300 relative p-2 text-xl ml-auto">Twitter</button>
+          <button className="flex gap-2 rounded-lg border border-slate-200 px-4 py-2 text-slate-700 hover:border-slate-400 hover:shadow dark:border-slate-700 dark:text-slate-200 dark:hover:border-slate-500">
+            <Image
+              className="h-6 w-6"
+              src="something.svg"
+              loading="lazy"
+              alt="google logo"
+              width={6}
+              height={6}
+            />
+            <span>Login with something else</span>
+          </button>
         </div>
       </div>
     </main>
-  );
+  )
 }
