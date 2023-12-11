c12f85c8760c846437fc1638ac12184969e948a6
YechiamWem <yechiamwe@gmail.com>
2023-12-11 19:14:40 +0200

    Fix comment not receiving form data

--- cv_next/app/cv/[cvId]/components/commentForm/CommentFormRSC.tsx
+++ cv_next/app/cv/[cvId]/components/commentForm/CommentFormRSC.tsx
@@ -6,7 +6,7 @@ export const CommentFormRSC = () => {
         <div className="py-2 pl-4 pr-2 bg-white rounded-lg rounded-t-lg border border-gray-200
                         dark:bg-gray-800 dark:border-gray-700 grid grid-cols-[90%_10%]">
             <label htmlFor="comment" className="sr-only">Your comment</label>
-            <textarea id="comment" rows={2}
+            <textarea id="comment" name="comment" rows={2}
                 className="flex-col row-span-2 overflow-hidden resize-y min-h-[2.5rem] leading-5 px-0 w-full text-sm
                             text-gray-900 border-0 focus:ring-0 focus:outline-none mt-2
                             dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
