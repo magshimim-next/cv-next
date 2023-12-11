a9ab83385e3c882a8c3b5dd9fe143a45ac44bfc5
YechiamWem <yechiamwe@gmail.com>
2023-12-11 19:31:35 +0200

    add a small todo

--- cv_next/app/cv/[cvId]/components/commentForm/commentForm.tsx
+++ cv_next/app/cv/[cvId]/components/commentForm/commentForm.tsx
@@ -9,6 +9,8 @@ type CommentFormProps = {
     show: boolean
 }
 
+//TODO: think about how to connect a pending/loading state here to the loading of new data in comments.
+//Alternatively, we can show a loading state in the comments.. need to figure out how.
 export const CommentForm:React.FC<CommentFormProps> = ({ children, formAction, revalidatePath, show }) => {
 
     const formRef = useRef<HTMLFormElement | null>(null);
