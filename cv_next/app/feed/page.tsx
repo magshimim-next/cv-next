716a938b037446aea2be25f5f66b93acd42deb78
Arad Donenfeld <aradon267@gmail.com>
2023-12-29 14:55:00 +0200

    UPDATED: removed unimportant checks and example of how to access user data

--- cv_next/app/feed/page.tsx
+++ cv_next/app/feed/page.tsx
@@ -1,17 +1,12 @@
 import { Suspense } from "react";
 import { getServerSession } from "next-auth/next"
 import { options } from "../api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation"
-import { useSession } from 'next-auth/react'
 import { fetchCvs } from "@/app/actions/fetchCvs";
 import Feed from "./components/feed";
 
 export default async function Page() {
   const session = await getServerSession(options);
-  if (session?.user.role == "0")
-  {
-    redirect("/deactivated");
-  }
+  console.log(session?.user.email)
   const fetchedCvs = await fetchCvs({lastId: undefined});
 
   return (
