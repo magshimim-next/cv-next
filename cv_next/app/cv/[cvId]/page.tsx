2ff9deb4deee20a3b989af0b84cee53fc5d6cdfb
avnogy <avner.mindelis@gmail.com>
2024-02-18 19:10:40 +0200

    create cv preview page

---
+++ cv_next/app/cv/[cvId]/page.tsx
@@ -0,0 +1,29 @@
+"use server"
+
+import { notFound } from "next/navigation"
+import { getCvById } from "@/server/api/cvs"
+import { decodeValue } from "@/lib/utils"
+import { CvPreview } from "./components/cvPreview"
+
+export default async function Page({ params }: { params: { cvId: string } }) {
+  const { cvId } = params
+
+  const decodedCvId = decodeValue(decodeURIComponent(cvId))
+  if (!decodedCvId) {
+    notFound()
+  }
+
+  const cv = await getCvById(decodedCvId)
+
+  if (cv === null) {
+    notFound()
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-y-4 md:grid-cols-[65%_35%] md:gap-x-4">
+      <section className="h-[78.75rem] flex-col rounded-lg">
+        {cv ? <CvPreview cv={cv} /> : null}
+      </section>
+    </div>
+  )
+}
