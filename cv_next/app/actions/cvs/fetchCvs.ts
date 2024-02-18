9be646a1eae74d4fdf2ad2b757f33f08f439abbd
avnogy <avner.mindelis@gmail.com>
2024-02-18 21:42:32 +0200

    fix revalidation in feed and remove manual caching of cvs

--- cv_next/app/actions/cvs/fetchCvs.ts
+++ cv_next/app/actions/cvs/fetchCvs.ts
@@ -1,32 +1,11 @@
 "use server"
 
 import { getPaginatedCvs } from "@/server/api/cvs"
-import MyLogger from "@/server/base/logger"
-
-let mappedResults: { [key: number]: PaginatedCvsModel | null } = {}
 
 export const fetchCvsForFeed = async ({
   page,
-  forceReset = false,
 }: {
   page?: number
-  forceReset?: boolean
 }): Promise<PaginatedCvsModel | null> => {
-  if (forceReset) {
-    mappedResults = {}
-  }
-  if (page && mappedResults?.[page]) {
-    MyLogger.logDebug(
-      `result from cache for id: ${page}\ncache: ${Object.keys(
-        mappedResults
-      ).toString()}`
-    )
-    return mappedResults?.[page]
-  }
-
-  const result: PaginatedCvsModel | null = await getPaginatedCvs(true, page)
-  if (result) {
-    mappedResults[result.page] = result
-  }
-  return result
+  return await getPaginatedCvs(true, page)
 }
