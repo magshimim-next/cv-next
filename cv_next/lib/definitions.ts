64c851525be6caedf23e7ee59a751727a53cf416
Arad Donenfeld <aradon267@gmail.com>
2024-03-01 19:36:02 +0200

    FIXED: according to old CR notes

--- cv_next/lib/definitions.ts
+++ cv_next/lib/definitions.ts
@@ -1,3 +1,5 @@
+import { PageHeader } from "@/types/models/contents"
+
 export default class Definitions {
   public static readonly undefinedIndex = -1
   public static readonly CVS_PER_PAGE = 6
@@ -5,6 +7,16 @@ export default class Definitions {
   public static readonly MAX_OPERATIONS = 10
   public static readonly MAX_OPERATIONS_REFILL_PER_SECOND = 2
   public static readonly CVS_REVALIDATE_TIME_IN_SECONDS = 0
-  public static readonly NEXT_COOKIE_AUTH_NAME =
-    "sb-weepggwhsrfmizptbnua-auth-token"
+}
+
+export const heroHeader: PageHeader = {
+  header: `Landing pages made easy`,
+  subheader: `Easy to setup. Customizable. Quick. Responsive.`,
+  image: `/hero-img.webp`,
+}
+
+export const inactiveHeader: PageHeader = {
+  header: `Access Denied`,
+  subheader: `This page requieres that you get approved by the moderators`,
+  image: `/access-denied.webp`,
 }
