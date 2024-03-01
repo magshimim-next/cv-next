034c15d4d22aceff3ccc26e526b84cdc20aa0ec2
Arad Donenfeld <aradon267@gmail.com>
2024-03-01 21:00:48 +0200

    UPDATED: login screen and redirect | DELETED: login server action

--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -1,9 +1,19 @@
-import {login} from '../actions/login/gmailAuth'
+"use client"
+import { Auth } from '@supabase/auth-ui-react'
+import { useSupabase } from "@/hooks/supabaseHooks";
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 export default function Page() {
+  const supabase = useSupabase()
+
   return (
-    <form>
-      <button formAction={login}>Log in</button>
-    </form>
+    <Auth
+      supabaseClient={supabase}
+      appearance={{theme: ThemeSupa}}
+      theme='dark'
+      providers={['google']}
+      redirectTo='http://localhost:3000/auth/callback'
+      onlyThirdPartyProviders
+    />
   )
 }
