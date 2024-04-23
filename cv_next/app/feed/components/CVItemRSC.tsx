cadb151ea60c74e54d1851accafda2135c803615
Arad Donenfeld <aradon267@gmail.com>
2024-04-23 16:26:36 +0300

    UPDATED: ran prettier on some files

--- cv_next/app/feed/components/CVItemRSC.tsx
+++ cv_next/app/feed/components/CVItemRSC.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import generateImageUrl from "@/helpers/imageURLHelper"
 import Categories from "@/types/models/categories"
 import { useState } from "react"
-import blankCvImg from "@/public/images/blackCv.png";
+import blankCvImg from "@/public/images/blackCv.png"
 interface CVCardProps {
   cv: CvModel
 }
@@ -15,7 +15,7 @@ export default function CVItemRSC({ cv }: CVCardProps) {
   const imageUrl = generateImageUrl(cv.document_link)
   const formattedDate = new Date(cv.created_at).toLocaleDateString("en-US")
 
-  const [imgSrc, setImgSrc] = useState<StaticImageData|string>(imageUrl)
+  const [imgSrc, setImgSrc] = useState<StaticImageData | string>(imageUrl)
 
   const handleImgError = (e: any) => {
     e.stopPropagation()
@@ -33,19 +33,17 @@ export default function CVItemRSC({ cv }: CVCardProps) {
         alt="CV Preview"
         priority
       />
-      <div className="overlay backdrop-blur-[0.5px] gradient-blur-backdrop hover:via-transparent hover:backdrop-blur-none absolute bottom-0 flex h-full w-full rounded-lg transition">
+      <div className="overlay gradient-blur-backdrop absolute bottom-0 flex h-full w-full rounded-lg backdrop-blur-[0.5px] transition hover:via-transparent hover:backdrop-blur-none">
         <div className="overlay absolute bottom-0 h-1/5 w-full rounded-xl bg-transparent p-6">
           <div className="absolute bottom-0 left-0 mx-5 mb-2.5">
             <div className="mr-2 inline text-xl font-bold text-black">
               {cv.user_id}
             </div>
-            <p className="inline text-xs text-neutral-400">
-              {formattedDate}
-            </p>
+            <p className="inline text-xs text-neutral-400">{formattedDate}</p>
           </div>
           <Link
             href={categoryLink}
-            className="absolute bottom-0 right-0 mx-4 mb-2.5 rounded-full bg-gray-700 px-3 py-1 text-sm font-semibold text-white hover:underline hover:bg-gray-400"
+            className="absolute bottom-0 right-0 mx-4 mb-2.5 rounded-full bg-gray-700 px-3 py-1 text-sm font-semibold text-white hover:bg-gray-400 hover:underline"
           >
             #{Categories.category[cv.category_id]}
           </Link>
--- cv_next/app/feed/components/filterPanel.tsx
+++ cv_next/app/feed/components/filterPanel.tsx
@@ -5,42 +5,56 @@ import Categories from "@/types/models/categories"
 import { ModeSwitch } from "@/components/ui/filters/modeSwitch"
 
 export interface filterObj {
-    searchValue: string
-    categoryId: number | null
+  searchValue: string
+  categoryId: number | null
 }
 
-export const MODES:[string, string] = ["review", "published"]
+export const MODES: [string, string] = ["review", "published"]
 
-export const FilterPanel = ({defaultFilters, onChange, cvs}: {
-    defaultFilters: filterObj
-    cvs: CvModel[],
-    onChange: (filters: filterObj) => void
-  }) => {
-    const [searchValue, setSearchValue] = useState(defaultFilters.searchValue)
-    const [categoryId, setCategoryId] = useState(defaultFilters.categoryId)
-    const [modeValue, setModeValue] = useState(MODES[0])
+export const FilterPanel = ({
+  defaultFilters,
+  onChange,
+  cvs,
+}: {
+  defaultFilters: filterObj
+  cvs: CvModel[]
+  onChange: (filters: filterObj) => void
+}) => {
+  const [searchValue, setSearchValue] = useState(defaultFilters.searchValue)
+  const [categoryId, setCategoryId] = useState(defaultFilters.categoryId)
+  const [modeValue, setModeValue] = useState(MODES[0])
 
-    useEffect(() => {
-        onChange({
-            categoryId: categoryId,
-            searchValue: searchValue
-        })
-    }, [searchValue, categoryId])
+  useEffect(() => {
+    onChange({
+      categoryId: categoryId,
+      searchValue: searchValue,
+    })
+  }, [searchValue, categoryId])
 
-    return (
-        <div className="mx-10 my-[10px] flex flex-row justify-between items-center gap-2">
-            <SearchBox placeHolder=" input text to search" value={searchValue} onChange={(value: string) => {setSearchValue(value)}}></SearchBox>
-            <DropdownInput 
-                placeHolder="all" 
-                valueIds={Object.keys(Categories.category).map(key=> parseInt(key)).filter(key => !isNaN(key))} 
-                text="catagory" 
-                valueId={categoryId} 
-                onChange={(value: number | null) => {setCategoryId(value)}}
-                getValueById={(id: number) => {return Categories.category[id]}}>
-
-            </DropdownInput>
-            {/* <ModeSwitch value={modeValue} values={MODES} onChange={setModeValue}></ModeSwitch> */}
-        </div>
-    )
-  }
-  
+  return (
+    <div className="mx-10 my-[10px] flex flex-row items-center justify-between gap-2">
+      <SearchBox
+        placeHolder=" input text to search"
+        value={searchValue}
+        onChange={(value: string) => {
+          setSearchValue(value)
+        }}
+      ></SearchBox>
+      <DropdownInput
+        placeHolder="all"
+        valueIds={Object.keys(Categories.category)
+          .map((key) => parseInt(key))
+          .filter((key) => !isNaN(key))}
+        text="catagory"
+        valueId={categoryId}
+        onChange={(value: number | null) => {
+          setCategoryId(value)
+        }}
+        getValueById={(id: number) => {
+          return Categories.category[id]
+        }}
+      ></DropdownInput>
+      {/* <ModeSwitch value={modeValue} values={MODES} onChange={setModeValue}></ModeSwitch> */}
+    </div>
+  )
+}
