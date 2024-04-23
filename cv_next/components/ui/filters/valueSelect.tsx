5532319c09b946a7ed87ca0672c1db561367bf30
Arad Donenfeld <aradon267@gmail.com>
2024-04-23 18:14:45 +0300

    ADDED: precommit eslint settings

--- cv_next/components/ui/filters/valueSelect.tsx
+++ cv_next/components/ui/filters/valueSelect.tsx
@@ -1,45 +1,58 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const DropdownInput = ({
-    placeHolder,
-    valueIds,
-    valueId,
-    onChange,
-    getValueById,
-    text
-  }: {
-    placeHolder: string,
-    valueIds: number[],
-    valueId: number | null,
-    getValueById: (id: number) => string
-    onChange: (newValue: number | null) => void,
-    text: string
-  }) => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const isPlaceHolder = valueId === null
+  placeHolder,
+  valueIds,
+  valueId,
+  onChange,
+  getValueById,
+  text,
+}: {
+  placeHolder: string;
+  valueIds: number[];
+  valueId: number | null;
+  getValueById: (id: number) => string;
+  onChange: (newValue: number | null) => void;
+  text: string;
+}) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isPlaceHolder = valueId === null;
 
-    const changeIsMenuOpen = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+  const changeIsMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return <>
-        <div className={`flex justify-center items-center relative w-1/6 h-2/6 whitespace-nowrap bg-white outline-gray-40 outline-2 ${isMenuOpen ? "outline-b-0 rounded-b-none" : ""} px-10 py-4 rounded-md cursor-pointer box-border`} onClick={changeIsMenuOpen} >
-            <div className={`${isPlaceHolder ? 'text-gray-400' : 'text-black'}`}>
-                {`${text}: ${isPlaceHolder ? placeHolder : getValueById(valueId)}`}
-            </div>
-            <div className={`absolute z-10 top-full w-full h-fit bg-white block ${isMenuOpen ? "block" : "hidden"} max-h-40 overflow-y-auto border-gray-40 border-2 border-t-0 divide-y-2`}>
-                <div className="text-gray-400 w-full h-10 bg-white hover:bg-slate-200 flex justify-center items-center" onClick={() => onChange(null)}> 
-                    {"all"}
-                </div>
-                {
-                    valueIds.map((possibleValueId) => {
-                        return <div className="text-black w-full h-8 bg-white hover:bg-slate-200 flex justify-center items-center" onClick={() => onChange(possibleValueId)}> 
-                            {`${getValueById(possibleValueId)}`}
-                        </div>
-                    })
-                }
-            </div>
+  return (
+    <>
+      <div
+        className={`outline-gray-40 relative flex h-2/6 w-1/6 items-center justify-center whitespace-nowrap bg-white outline-2 ${isMenuOpen ? "outline-b-0 rounded-b-none" : ""} box-border cursor-pointer rounded-md px-10 py-4`}
+        onClick={changeIsMenuOpen}
+      >
+        <div className={`${isPlaceHolder ? "text-gray-400" : "text-black"}`}>
+          {`${text}: ${isPlaceHolder ? placeHolder : getValueById(valueId)}`}
         </div>
+        <div
+          className={`absolute top-full z-10 block h-fit w-full bg-white ${isMenuOpen ? "block" : "hidden"} border-gray-40 max-h-40 divide-y-2 overflow-y-auto border-2 border-t-0`}
+        >
+          <div
+            className="flex h-10 w-full items-center justify-center bg-white text-gray-400 hover:bg-slate-200"
+            onClick={() => onChange(null)}
+          >
+            {"all"}
+          </div>
+          {valueIds.map((possibleValueId) => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div
+                className="flex h-8 w-full items-center justify-center bg-white text-black hover:bg-slate-200"
+                onClick={() => onChange(possibleValueId)}
+              >
+                {`${getValueById(possibleValueId)}`}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
-  }
-  
+  );
+};
