130e81b036896a601b151511abb5293607c79272
Arad Donenfeld <aradon267@gmail.com>
2024-04-27 16:10:20 +0300

    UPDATED: next param is no longer used, hardcoded /feed instead

--- cv_next/app/auth/callback/route.ts
+++ cv_next/app/auth/callback/route.ts
@@ -6,8 +6,6 @@ import { type CookieOptions, createServerClient } from "@supabase/ssr"
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get("code")
-  // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/"
   if (code) {
     const cookieStore = cookies()
     const supabase = createServerClient(
@@ -29,7 +27,7 @@ export async function GET(request: Request) {
     )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}feed`)
+      return NextResponse.redirect(`${origin}/feed`)
     }
   }
 
