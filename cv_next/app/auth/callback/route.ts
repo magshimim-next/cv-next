833060a26d2ed75bdc4808c9b3b16c9612594bd1
Arad Donenfeld <aradon267@gmail.com>
2024-04-28 01:34:44 +0300

    UPDATED: redirects moved definitions

--- cv_next/app/auth/callback/route.ts
+++ cv_next/app/auth/callback/route.ts
@@ -1,36 +1,39 @@
-"use server"
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
-import { type CookieOptions, createServerClient } from "@supabase/ssr"
+"use server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import Definitions from "@/lib/definitions";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
-  const code = searchParams.get("code")
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
   if (code) {
-    const cookieStore = cookies()
+    const cookieStore = cookies();
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
         cookies: {
           get(name: string) {
-            return cookieStore.get(name)?.value
+            return cookieStore.get(name)?.value;
           },
           set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options })
+            cookieStore.set({ name, value, ...options });
           },
           remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options })
+            cookieStore.delete({ name, ...options });
           },
         },
       }
-    )
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}/feed`)
+      return NextResponse.redirect(
+        `${origin}` + Definitions.AUTH_DEFAULT_REDIRECT
+      );
     }
   }
 
   // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+  return NextResponse.redirect(`${origin}/`); //TODO: move to regular error page
 }
--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -1,18 +1,20 @@
-"use client"
-import { Auth } from "@supabase/auth-ui-react"
-import { useSupabase } from "@/hooks/supabase"
-import { ThemeSupa } from "@supabase/auth-ui-shared"
-import Definitions from "@/lib/definitions"
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { useSupabase } from "@/hooks/supabase";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import Definitions from "@/lib/definitions";
 export default function Page() {
-  const supabase = useSupabase()
+  const supabase = useSupabase();
   return (
     <Auth
       supabaseClient={supabase}
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       providers={["google"]}
-      redirectTo={process.env.NEXT_PUBLIC_BASE_URL + Definitions.AUTH_REDIRECT}
+      redirectTo={
+        process.env.NEXT_PUBLIC_BASE_URL + Definitions.AUTH_CALLBACK_REDIRECT
+      }
       onlyThirdPartyProviders
     />
-  )
+  );
 }
