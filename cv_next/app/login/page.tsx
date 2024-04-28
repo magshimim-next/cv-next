26365045a6b71d6d96d887285cbc6c74716d722a
Arad Donenfeld <aradon267@gmail.com>
2024-04-28 16:06:37 +0300

    UPDATED: login to use the theme

--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -3,13 +3,16 @@ import { Auth } from "@supabase/auth-ui-react";
 import { useSupabase } from "@/hooks/supabase";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import Definitions from "@/lib/definitions";
+import { useTheme } from "next-themes";
+
 export default function Page() {
   const supabase = useSupabase();
+  const { theme } = useTheme();
   return (
     <Auth
       supabaseClient={supabase}
       appearance={{ theme: ThemeSupa }}
-      theme="dark"
+      theme={theme}
       providers={["google"]}
       redirectTo={
         process.env.NEXT_PUBLIC_BASE_URL + Definitions.AUTH_CALLBACK_REDIRECT
