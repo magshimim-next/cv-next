1cd22a0e9689d1b27a6a6315aa4e71c2c25bf50f
Arad Donenfeld <aradon267@gmail.com>
2024-04-28 14:50:53 +0300

    UPDATED: prettier

--- cv_next/app/global.d.ts
+++ cv_next/app/global.d.ts
@@ -1,14 +1,14 @@
-import { Database as DB } from "@/types/database.types"
+import { Database as DB } from "@/types/database.types";
 
 declare global {
-  type Database = DB
-  type CvModel = DB["public"]["Tables"]["cvs"]["Row"]
-  type PaginatedCvsModel = { page: number; cvs: CvModel[] }
-  type CommentModel = DB["public"]["Tables"]["comments"]["Row"]
-  type UserModel = DB["public"]["Tables"]["users"]["Row"]
+  type Database = DB;
+  type CvModel = DB["public"]["Tables"]["cvs"]["Row"];
+  type PaginatedCvsModel = { page: number; cvs: CvModel[] };
+  type CommentModel = DB["public"]["Tables"]["comments"]["Row"];
+  type UserModel = DB["public"]["Tables"]["users"]["Row"];
   type PageHeader = {
-    header: string
-    subheader: string
-    image: string
-  }
+    header: string;
+    subheader: string;
+    image: string;
+  };
 }
