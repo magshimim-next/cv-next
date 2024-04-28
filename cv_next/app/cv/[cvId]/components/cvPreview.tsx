3b995586360e754a7f6b5c7b87622bc22fef7c4f
Arad Donenfeld <aradon267@gmail.com>
2024-04-28 16:48:56 +0300

    ADDED: full view of CV

--- cv_next/app/cv/[cvId]/components/cvPreview.tsx
+++ cv_next/app/cv/[cvId]/components/cvPreview.tsx
@@ -1,17 +1,11 @@
-import generateImageUrl from "@/helpers/imageURLHelper"
-import Image from "next/image"
-
 export const CvPreview = ({ cv }: { cv: CvModel }) => {
-  const imageUrl = generateImageUrl(cv.document_link)
   return (
-    <Image
-      width={0}
-      height={0}
-      className="h-full w-full flex-1 rounded-lg"
-      src={imageUrl}
-      alt="CV Preview"
-      priority
-      referrerPolicy="no-referrer"
-    />
-  )
-}
+    <div>
+      <iframe
+        src={cv.document_link + "&rm=minimal"}
+        width="100%"
+        height="800"
+      />
+    </div>
+  );
+};
