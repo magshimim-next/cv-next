4c2d8b687b6ad10051963867836c94815eecdc85
Arad Donenfeld <aradon267@gmail.com>
2024-04-28 15:14:29 +0300

    REMOVED: entire part that was hidden in navbar

--- cv_next/components/layout/navbar.tsx
+++ cv_next/components/layout/navbar.tsx
@@ -42,44 +42,6 @@ export default function Navbar() {
                     CV-NEXT
                   </h1>
                 </Link>
-                <div className="flex gap-1 md:hidden">
-                  <button
-                    className="rounded-md p-2 text-primary outline-none focus:border focus:border-primary"
-                    aria-label="Hamburger Menu"
-                    onClick={() => setIsNavbarMenuOpen(!isNavbarMenuOpen)}
-                  >
-                    {isNavbarMenuOpen ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 "
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 "
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M4 6h16M4 12h16M4 18h16"
-                        />
-                      </svg>
-                    )}
-                  </button>
-                  <ModeToggle />
-                </div>
               </div>
             </div>
             <div className="reletive">
