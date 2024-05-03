f65d348e462766c486ee872138b91fd97fa8a9e1
Arad Donenfeld <aradon267@gmail.com>
2024-05-03 22:37:03 +0300

    UPDATED: popup no uses regular dialog and has click outside to close

--- cv_next/components/layout/navbar/popup.tsx
+++ cv_next/components/layout/navbar/popup.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import closeIcon from "@/public/images/closeIcon.png";
+import { useRef, useEffect } from "react";
 import Link from "next/link";
 import { useSupabase } from "@/hooks/supabase";
 
@@ -21,6 +22,12 @@ interface PopupProps {
 }
 
 export default function Popup({ closeCb }: PopupProps) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  useEffect(() => {
+    if (dialogRef.current) {
+      dialogRef.current.show();
+    }
+  }, []);
   const supabase = useSupabase();
 
   const handleSelection = (route: string) => {
@@ -31,6 +38,38 @@ export default function Popup({ closeCb }: PopupProps) {
   };
 
   return (
+    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center">
+      <div
+        className="absolute h-full w-full bg-black opacity-20"
+        onClick={closeCb}
+      ></div>
+
+      <dialog
+        ref={dialogRef}
+        className="relative h-[500px] w-[400px] rounded-xl border-2 border-black backdrop-blur-2xl"
+      >
+        <div
+          className="absolute left-5 top-5 h-7 w-7 cursor-pointer"
+          onClick={closeCb}
+        >
+          <Image alt="closeIcon" src={closeIcon}></Image>
+        </div>
+        <ul className="mt-10 flex w-full flex-col items-center">
+          {navLinks.map((link) => (
+            <li key={link.route}>
+              <Link
+                className="text-lg font-medium hover:underline"
+                href={link.path}
+                onClick={() => handleSelection(link.route)}
+              >
+                {link.route}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </dialog>
+    </div>
+    /*
     <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center">
       <div className="absolute h-full w-full bg-black opacity-20"></div>
       <div className="relative h-[500px] w-[400px] rounded-xl border-2 border-black backdrop-blur-2xl">
@@ -54,6 +93,6 @@ export default function Popup({ closeCb }: PopupProps) {
           ))}
         </ul>
       </div>
-    </div>
+    </div>*/
   );
 }
