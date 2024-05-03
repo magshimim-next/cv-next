b441be64beb699e541c79c036d278c650034b39e
Arad Donenfeld <aradon267@gmail.com>
2024-05-03 22:55:18 +0300

    UPDATED: deleted commented code

--- cv_next/components/layout/navbar/popup.tsx
+++ cv_next/components/layout/navbar/popup.tsx
@@ -69,30 +69,5 @@ export default function Popup({ closeCb }: PopupProps) {
         </ul>
       </dialog>
     </div>
-    /*
-    <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center">
-      <div className="absolute h-full w-full bg-black opacity-20"></div>
-      <div className="relative h-[500px] w-[400px] rounded-xl border-2 border-black backdrop-blur-2xl">
-        <div
-          className="absolute left-5 top-5 h-7 w-7 cursor-pointer"
-          onClick={closeCb}
-        >
-          <Image alt="closeIcon" src={closeIcon}></Image>
-        </div>
-        <ul className="mt-10 flex w-full flex-col items-center">
-          {navLinks.map((link) => (
-            <li key={link.route}>
-              <Link
-                className="text-lg font-medium text-white hover:underline"
-                href={link.path}
-                onClick={() => handleSelection(link.route)}
-              >
-                {link.route}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>*/
   );
 }
