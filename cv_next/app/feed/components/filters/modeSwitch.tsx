acda4e5f935205cd4003d6e88f320fe7f36f26e0
Arad Donenfeld <aradon267@gmail.com>
2024-05-03 16:24:40 +0300

    ADDED: TODO in mode switch

--- cv_next/app/feed/components/filters/modeSwitch.tsx
+++ cv_next/app/feed/components/filters/modeSwitch.tsx
@@ -1,26 +1,30 @@
-
+// TODO: talk with Yechiam about this entire file for the next version
 export const ModeSwitch = ({
-    values,
-    value,
-    onChange,
-  }: {
-    value: string,
-    values: [string, string],
-    onChange: (newValue: string) => void,
-  }) => {
-    const isRightSide = value === values[1]
+  values,
+  value,
+  onChange,
+}: {
+  value: string;
+  values: [string, string];
+  onChange: (newValue: string) => void;
+}) => {
+  const isRightSide = value === values[1];
 
-    const onClick = () => {
-        onChange(isRightSide ? values[0] : values[1])
-    }
+  const onClick = () => {
+    onChange(isRightSide ? values[0] : values[1]);
+  };
 
-    return <>
-        <div className={`flex justify-center items-center relative w-1/6 h-3/6 whitespace-nowrap bg-white border-gray-40 border-2 px-10 py-4 rounded-md cursor-pointer box-border`} onClick={onClick}>
-            <div className='text-black'>
-                {value}
-            </div>
-            <div className={`absolute h-full w-5 bg-black rounded-md ${isRightSide ? "right-0" : `left-0`} transition-transform`}></div>
-        </div>
+  return (
+    <>
+      <div
+        className={`border-gray-40 relative box-border flex h-3/6 w-1/6 cursor-pointer items-center justify-center whitespace-nowrap rounded-md border-2 bg-white px-10 py-4`}
+        onClick={onClick}
+      >
+        <div className="text-black">{value}</div>
+        <div
+          className={`absolute h-full w-5 rounded-md bg-black ${isRightSide ? "right-0" : `left-0`} transition-transform`}
+        ></div>
+      </div>
     </>
-  }
-  
+  );
+};
