ef2adf54e094ecbf8ce72065a6818f965fe8db03
Arad Donenfeld <aradon267@gmail.com>
2024-05-09 19:00:47 +0300

    UPDATED: ignore eslint rule for exaustive deps as the onChange doesnt change

--- cv_next/app/feed/components/filterPanel.tsx
+++ cv_next/app/feed/components/filterPanel.tsx
@@ -27,6 +27,7 @@ export const FilterPanel = ({
       categoryId: categoryId,
       searchValue: searchValue,
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchValue, categoryId]);
 
   const mapCategories: number[] = useMemo(() => {
