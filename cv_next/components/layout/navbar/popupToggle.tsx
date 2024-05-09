72848cef50ae8908538edcb711f6b870e0735a9f
Arad Donenfeld <aradon267@gmail.com>
2024-05-09 17:55:52 +0300

    UPDATED: settigns is now profilepopup

--- cv_next/components/layout/navbar/popupToggle.tsx
+++ cv_next/components/layout/navbar/popupToggle.tsx
@@ -1,25 +1,25 @@
 "use client";
-import settignsIcon from "@/public/images/settigns.png";
+import profileIcon from "@/public/images/profile.png";
 import Image from "next/image";
 import { useState } from "react";
 import Popup from "./popup";
 
 export function PopupToggle() {
-  const [isSettignsOpen, setIsSettignsOpen] = useState(false);
+  const [isProfilePopupOpen, setIsProfilePopupOpen] = useState(false);
 
   return (
     <div>
       <div className="cursor-pointer rounded-full hover:bg-[#27374e]">
         <Image
-          alt="settings"
+          alt="profile"
           height={40}
           width={40}
-          src={settignsIcon}
-          onClick={() => setIsSettignsOpen(!isSettignsOpen)}
+          src={profileIcon}
+          onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}
         ></Image>
       </div>
-      {isSettignsOpen && (
-        <Popup closeCb={() => setIsSettignsOpen(false)}></Popup>
+      {isProfilePopupOpen && (
+        <Popup closeCb={() => setIsProfilePopupOpen(false)}></Popup>
       )}
     </div>
   );
