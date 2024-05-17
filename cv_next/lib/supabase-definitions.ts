0d34d047ca800fd341d4e7635a23909d50e119c9
Arad Donenfeld <aradon267@gmail.com>
2024-05-18 02:42:34 +0300

    UPDATED: profile keys

--- cv_next/lib/supabase-definitions.ts
+++ cv_next/lib/supabase-definitions.ts
@@ -14,3 +14,17 @@ export const CvKeys = {
   resolved: "resolved",
   user_id: "user_id",
 };
+
+export const ProfileKeys = {
+  avatar_url: "avatar_url",
+  full_name: "full_name",
+  updated_at: "updated_at",
+  user_type: "user_type",
+  username: "username",
+  id: "id",
+  user_types: {
+    inactive: "inactive",
+    active: "active",
+    admin: "admin",
+  },
+};
