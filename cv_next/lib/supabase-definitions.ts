701b4fb429ddb8cae527367c32d768ea76a2f0bc
Arad Donenfeld <aradon267@gmail.com>
2024-05-18 02:36:15 +0300

    UPDATED: now export supabase differently

--- cv_next/lib/supabase-definitions.ts
+++ cv_next/lib/supabase-definitions.ts
@@ -1,18 +1,16 @@
-export default class SupabaseDefinitions {
-  public static readonly Tables: {
-    cvs: "cvs";
-    comments: "comments";
-    profiles: "profiles";
-  };
+export const Tables = {
+  cvs: "cvs",
+  comments: "comments",
+  profiles: "profiles",
+};
 
-  public static readonly CvKeys: {
-    category_id: "category_id";
-    created_at: "created_at";
-    deleted: "deleted";
-    description: "description";
-    document_link: "document_link";
-    id: "id";
-    resolved: "resolved";
-    user_id: "user_id";
-  };
-}
+export const CvKeys = {
+  category_id: "category_id",
+  created_at: "created_at",
+  deleted: "deleted",
+  description: "description",
+  document_link: "document_link",
+  id: "id",
+  resolved: "resolved",
+  user_id: "user_id",
+};
