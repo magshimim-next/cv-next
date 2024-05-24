87a8b9c27457dfa91afc967b138f77a1c6aff173
Arad Donenfeld <aradon267@gmail.com>
2024-05-24 14:47:15 +0300

    UPDATED: placeholder for feed placement, user image and name

--- cv_next/app/profile/[profileId]/page.tsx
+++ cv_next/app/profile/[profileId]/page.tsx
@@ -1,7 +1,11 @@
 "use server";
 
 import { notFound } from "next/navigation";
-import { getUserById } from "@/server/api/users";
+import { getUser } from "@/app/actions/users/getUser";
+//import Feed from "../../feed/components/feed";
+import Image from "next/image";
+import profileIcon from "@/public/images/profile.png";
+
 /*import { CvPreview } from "./components/cvPreview";
 import CommentsSection from "./components/commentSection/commentsSection";
 import CommentForm from "./components/commentSection/commentForm";
@@ -14,26 +18,45 @@ export default async function Page({
 }) {
   const { profileId } = params;
 
-  const profile = await getUserById(profileId);
+  const result = await getUser(profileId);
 
-  if (profile === null) {
+  if (result === null || !result.ok) {
     notFound();
   }
 
   return (
-    <div>hey</div>
-    /*<div>
-      <CvData cv={cv} />
-      <div className="grid grid-cols-1 gap-y-4 md:grid-cols-[65%_35%] md:gap-x-4">
+    <div>
+      <div className="grid grid-cols-1 gap-y-4 md:grid-cols-[40%_60%] md:gap-x-4">
         <section className="h-[78.75rem] flex-col rounded-lg">
-          {cv ? <CvPreview cv={cv} /> : null}
+          {/* TODO: add a filter that will be based on the user, remove filter bar*/}
+          {/*<Feed />*/}
         </section>
 
         <section className="h-[78.75rem] flex-col self-start">
-          <CommentForm cv={cv} />
-          <CommentsSection cv={cv} />
+          <div
+            className={`mb-3 rounded-lg border-b border-gray-200 bg-white p-6 text-base dark:bg-theme-800`}
+            style={{ height: "150px" }}
+          >
+            <div className="flex flex-col items-center">
+              <div className="flex justify-center">
+                <Image
+                  alt="profile"
+                  src={result.val.avatar_url || profileIcon}
+                  width={90}
+                  height={60 * 1.4142}
+                  className="rounded-lg p-2"
+                ></Image>
+              </div>
+              <div className="flex justify-center">
+                <p className="mr-3 inline-flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  {result.val.full_name || result.val.username}
+                </p>
+              </div>
+            </div>
+            {/*<p className="text-gray-500 dark:text-gray-400">{comment.data}</p>*/}
+          </div>
         </section>
       </div>
-    </div>*/
+    </div>
   );
 }
