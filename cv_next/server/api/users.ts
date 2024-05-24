791de1707e6e0cb0172add27fee9791866d2cfae
Arad Donenfeld <aradon267@gmail.com>
2024-05-24 18:34:36 +0300

    ADDED: definitions, setUserName

--- cv_next/server/api/users.ts
+++ cv_next/server/api/users.ts
@@ -1,30 +1,56 @@
-import "server-only"
+import "server-only";
 
-import SupabaseHelper from "./supabaseHelper"
-import { Ok, Err } from "@/lib/utils"
+import SupabaseHelper from "./supabaseHelper";
+import { Ok, Err } from "@/lib/utils";
+import { Tables, ProfileKeys } from "@/lib/supabase-definitions";
 
 export async function getUserById(
   userId: string
 ): Promise<Result<UserModel, string>> {
   try {
     const { data: user, error } = await SupabaseHelper.getSupabaseInstance()
-      .from("profiles")
+      .from(Tables.profiles)
       .select("*")
-      .eq("id", userId)
+      .eq(ProfileKeys.id, userId);
 
     if (error) {
-      return Err("Error @ " + getUserById.name + "\n", error)
+      return Err("Error @ " + getUserById.name + "\n", error);
     }
 
     if (!user || user.length !== 1) {
       return Err(
         "Expected only one match for query; users found: " +
           (user ? user.length : 0)
-      )
+      );
     }
 
-    return Ok(user[0] as UserModel)
+    return Ok(user[0] as UserModel);
   } catch (error) {
-    return Err("Error @ " + getUserById.name + "\n" + error)
+    return Err("Error @ " + getUserById.name + "\n" + error);
+  }
+}
+
+/**
+ * Changes the username of a given user.
+ *
+ * @param {string} userId - The ID of the user to update.
+ * @param {string} newUserName - The new username.
+ * @return {Promise<Result<void, string>>} A promise that resolves with void or rejects with an error message.
+ */
+export async function setUserName(
+  userId: string,
+  newUserName: string
+): Promise<Result<void, string>> {
+  try {
+    const { error } = await SupabaseHelper.getSupabaseInstance()
+      .from(Tables.profiles)
+      .update({ username: newUserName })
+      .eq(ProfileKeys.id, userId);
+    if (error) {
+      return Err(setUserName.name, error);
+    }
+    return Ok.EMPTY;
+  } catch (err) {
+    return Err(setUserName.name, undefined, err as Error);
   }
 }
