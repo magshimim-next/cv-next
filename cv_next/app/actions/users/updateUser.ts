20b2ffa81e5f6ea52e1dded79915eb7558caec77
Arad Donenfeld <aradon267@gmail.com>
2024-05-24 18:38:17 +0300

    ADDED: update username server action

---
+++ cv_next/app/actions/users/updateUser.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { setUserName } from "@/server/api/users";
+import { Err } from "@/lib/utils";
+import { logErrorWithTrace } from "@/server/base/logger";
+
+/**
+ * Updates a user's username.
+ *
+ * @param {string} userId - The ID of the user to update
+ * @param {string} newUserName - The new username
+ * @return {Promise<Result<void, string>>} A Promise with the result of the operation.
+ */
+export const setNewUsername = async (
+  userId: string,
+  newUserName: string
+): Promise<Result<void, string>> => {
+  const result = await setUserName(userId, newUserName);
+  if (result.ok) {
+    return result;
+  } else {
+    logErrorWithTrace(result);
+    return Err(
+      "An error has occurred while fetching the user data. Please try again later."
+    );
+  }
+};
