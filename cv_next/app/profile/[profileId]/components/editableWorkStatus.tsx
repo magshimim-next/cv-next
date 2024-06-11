9b29bfdf93e0587a6eba38111322a96cf2ec1641
Arad Donenfeld <aradon267@gmail.com>
2024-06-11 15:40:53 +0300

    UPDATED: users can now update categories of work status(no supa yet)

--- cv_next/app/profile/[profileId]/components/editableWorkStatus.tsx
+++ cv_next/app/profile/[profileId]/components/editableWorkStatus.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Check, X } from "lucide-react";
 import { useSupabase } from "@/hooks/supabase";
 import { setNewWorkStatus } from "@/app/actions/users/updateUser";
@@ -7,9 +7,18 @@ import Categories from "@/types/models/categories";
 import { ProfileKeys } from "@/lib/supabase-definitions";
 
 export default function EditableWorkStatus({ user }: { user: UserModel }) {
-  const [tempWorkStatus, setTempWorkStatus] = useState(user.work_status);
   const [viewingCurrentUser, setViewingCurrentUser] = useState(false);
+
   const [workStatus, setWorkStatus] = useState(user.work_status);
+  const [tempWorkStatus, setTempWorkStatus] = useState(user.work_status);
+
+  const [workCategories, setWorkCategories] = useState(
+    user.work_status_categories
+  );
+  const [tempWorkCategories, setTempWorkCategories] = useState(
+    user.work_status_categories || []
+  );
+
   const supabase = useSupabase();
 
   useEffect(() => {
@@ -35,7 +44,14 @@ export default function EditableWorkStatus({ user }: { user: UserModel }) {
     })();
   }, [workStatus, user, viewingCurrentUser]);
 
-  const workCategories = user.work_status_categories?.map(
+  const mapCategories: number[] = useMemo(() => {
+    const keys = Object.keys(Categories.category)
+      .map((key) => parseInt(key))
+      .filter((key) => !isNaN(key));
+    return keys;
+  }, []);
+
+  const workCategoriesComponent = tempWorkCategories.map(
     (categoryId, index) => (
       <span
         key={index}
@@ -46,30 +62,65 @@ export default function EditableWorkStatus({ user }: { user: UserModel }) {
     )
   );
 
-  if (workCategories && workCategories.length < 3 && viewingCurrentUser) {
-    const emptySpans = new Array(3 - workCategories.length).fill(
-      <span
-        key={`empty-${workCategories.length + 1}`}
-        className="right-0 mx-4 mb-2 justify-center rounded-full bg-gray-700 px-3 py-1 text-sm font-semibold text-white hover:bg-gray-400"
-      >
-        Empty
-      </span>
-    );
-    workCategories.push(...emptySpans);
-  }
-
   if (!viewingCurrentUser && user.work_status != "nothing") {
     return (
       <div className="flex-col items-center">
         <div className="mb-2 flex justify-center text-base">
           {user.username || user.full_name} is {workStatus}!
         </div>
-        <div className="flex justify-center text-base">{workCategories}</div>
+        <div className="flex justify-center text-base">
+          {workCategoriesComponent}
+        </div>
       </div>
     );
   }
 
   if (viewingCurrentUser) {
+    const filledWorkStatusCategories = [...tempWorkCategories];
+
+    while (filledWorkStatusCategories.length < 3) {
+      filledWorkStatusCategories.push(-1);
+    }
+
+    const editableCategories = filledWorkStatusCategories
+      .slice(0, 3)
+      .map((categoryId, index) => (
+        <select
+          key={index}
+          onChange={(e) => {
+            const newCategoryKey = e.target.value;
+            const newCategoryIndex =
+              Categories.category[
+                newCategoryKey as keyof typeof Categories.category
+              ];
+            if (newCategoryIndex !== undefined) {
+              const updatedCategories = [...tempWorkCategories];
+              updatedCategories[index] = newCategoryIndex;
+              setTempWorkCategories(updatedCategories);
+            }
+          }}
+          className="dark:bg-theme-800"
+          style={{
+            marginLeft: "5px",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+          value={Categories.category[categoryId]}
+        >
+          {mapCategories.map((value) => (
+            <option
+              key={value}
+              disabled={
+                tempWorkCategories?.includes(value) &&
+                value != Categories.category.Undefined
+              }
+            >
+              {Categories.category[value]}
+            </option>
+          ))}
+        </select>
+      ));
+
     return (
       <div className="flex-col items-center ">
         <div className="mb-2 flex justify-center text-base">
@@ -117,7 +168,7 @@ export default function EditableWorkStatus({ user }: { user: UserModel }) {
         </div>
         <div className="flex justify-center text-base">
           {user.work_status != "nothing" || workStatus != "nothing"
-            ? workCategories
+            ? editableCategories
             : "Update your work status and share it with the community!"}
         </div>
       </div>
