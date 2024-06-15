fca980304439d064d8e74a5d51c3771202ebce18
Arad Donenfeld <aradon267@gmail.com>
2024-06-15 19:41:01 +0300

    FIXED: JSDoc

--- cv_next/helpers/blobHelper.ts
+++ cv_next/helpers/blobHelper.ts
@@ -1,7 +1,8 @@
-/*
+/**
  * Calculate hash of a blob
- * @Params: blob
- * @Returns: a promised string containing the calculated hash
+ *
+ * @param {blob} blob - The blob to hash
+ * @return {Promise<string>} a promised string containing the calculated hash
  */
 async function calculateHash(blob: Blob): Promise<string> {
   const buffer = await blob.arrayBuffer();
@@ -13,10 +14,12 @@ async function calculateHash(blob: Blob): Promise<string> {
   return hashHex;
 }
 
-/*
+/**
  * Comparse 2 blobs using their hash
- * @Params: 2 blob
- * @Returns: a promised boolean that says if the hashes are the same or not
+ *
+ * @param {blob} blob1 - The first blob to compare
+ * @param {blob} blob2 - The second blob to compare
+ * @return {Promise<boolean>} a promised boolean that says if the hashes are the same or not
  */
 export async function compareHashes(
   blob1: Blob,
