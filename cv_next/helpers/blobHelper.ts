705c881726719aa7377692c66687cfc0dcce4f2e
Arad Donenfeld <aradon267@gmail.com>
2024-06-15 19:36:29 +0300

    UPDATED: JSDoc for the blob helper

--- cv_next/helpers/blobHelper.ts
+++ cv_next/helpers/blobHelper.ts
@@ -1,12 +1,27 @@
+/*
+ * Calculate hash of a blob
+ * @Params: blob
+ * @Returns: a promised string containing the calculated hash
+ */
 async function calculateHash(blob: Blob): Promise<string> {
   const buffer = await blob.arrayBuffer();
-  const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", buffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+  const hashHex = hashArray
+    .map((byte) => byte.toString(16).padStart(2, "0"))
+    .join("");
   return hashHex;
 }
 
-export async function compareHashes(blob1: Blob, blob2: Blob): Promise<boolean> {
+/*
+ * Comparse 2 blobs using their hash
+ * @Params: 2 blob
+ * @Returns: a promised boolean that says if the hashes are the same or not
+ */
+export async function compareHashes(
+  blob1: Blob,
+  blob2: Blob
+): Promise<boolean> {
   const hash1 = await calculateHash(blob1);
   const hash2 = await calculateHash(blob2);
   return hash1 === hash2;
