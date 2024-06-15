31c56738c4ac659a0e2961eb62781589b4364308
Arad Donenfeld <aradon267@gmail.com>
2024-06-15 20:04:08 +0300

    UPDATED: JSDoc for getBlurCv

--- cv_next/app/actions/cvs/getBlurCv.ts
+++ cv_next/app/actions/cvs/getBlurCv.ts
@@ -2,6 +2,12 @@
 import { getPlaiceholder } from "plaiceholder";
 import Definitions from "@/lib/definitions";
 
+/**
+ * Get a blurred version of a cv
+ *
+ * @param {string} cvId - The cv to get a blurred version of
+ * @return {Promise<string>} a promised string with the base64 of the blur
+ */
 export async function getBlurredCv(cvLink: string): Promise<string> {
   const resp = await fetch(cvLink, {
     next: { revalidate: Definitions.FETCH_WAIT_TIME },
