2a7bd229629bc46bfd02218e29bce97ebd5b3ae6
Arad Donenfeld <aradon267@gmail.com>
2024-06-21 14:20:30 +0300

    UPDATED: using react icon for default profile on navbar itself

--- cv_next/components/layout/navbar/popupToggle.tsx
+++ cv_next/components/layout/navbar/popupToggle.tsx
@@ -1,34 +1,35 @@
 "use client";
-import defaultProfileIcon from "@/public/images/profile.png";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 import Popup from "./popup";
 import { useSupabase } from "@/hooks/supabase";
 import { getUserFromId } from "@/app/actions/user/fetchUserInfo";
+import { FaUserCircle } from "react-icons/fa";
 
 export function PopupToggle() {
-  const [profileImage, setProfileImage] = useState<StaticImageData | string>(
-    defaultProfileIcon
-  );
+  const [profileImage, setProfileImage] = useState<string>("");
   const [userData, setUserData] = useState<UserModel | null>(null);
   const [signedIn, setSignIn] = useState(false);
   const supabase = useSupabase();
+  const defaultProfileIcon = (
+    <FaUserCircle size={40} style={{ color: "#FFF" }} />
+  );
 
   useEffect(() => {
     const fetchUser = async () => {
       const { data: connectedUser, error } = await supabase.auth.getUser();
       if (error || !connectedUser?.user) {
         setUserData(null);
-        setProfileImage(defaultProfileIcon);
+        setProfileImage("");
       } else {
         const currentUserObject = await getUserFromId(connectedUser.user.id);
         if (currentUserObject) {
           setUserData(currentUserObject);
-          setProfileImage(currentUserObject.avatar_url || defaultProfileIcon);
+          setProfileImage(currentUserObject.avatar_url || "");
           setSignIn(true);
         } else {
           setUserData(null);
-          setProfileImage(defaultProfileIcon);
+          setProfileImage("");
         }
       }
     };
@@ -39,13 +40,19 @@ export function PopupToggle() {
   return (
     <div>
       <div className="cursor-pointer rounded-full hover:bg-[#27374e]">
-        <Image
-          alt="profile"
-          height={40}
-          width={40}
-          src={profileImage}
-          onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}
-        ></Image>
+        {profileImage ? (
+          <Image
+            alt="profile"
+            height={40}
+            width={40}
+            src={profileImage}
+            onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}
+          ></Image>
+        ) : (
+          <div onClick={() => setIsProfilePopupOpen(!isProfilePopupOpen)}>
+            {defaultProfileIcon}
+          </div>
+        )}
       </div>
       {isProfilePopupOpen && (
         <Popup
