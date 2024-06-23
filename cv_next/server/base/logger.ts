72a336b5e0b4391919216697a7e9bef0b0390145
Arad Donenfeld <aradon267@gmail.com>
2024-06-23 23:17:25 +0300

    UPDATED: logger is now pino

--- cv_next/server/base/logger.ts
+++ cv_next/server/base/logger.ts
@@ -1,39 +1,8 @@
-/* eslint-disable no-console */
 import "server-only";
-import winston from "winston";
+import pino from "pino";
+import pretty from "pino-pretty";
 
-const logger = winston.createLogger({
-  level: "debug",
-});
+const stream = pretty({ colorize: true });
+const logger = pino(stream);
 
-const formatMeta = (meta: any) => {
-  const splat = meta[Symbol.for("splat")];
-  if (splat && splat.length) {
-    return splat.length === 1
-      ? JSON.stringify(splat[0])
-      : JSON.stringify(splat);
-  }
-  return "";
-};
-
-const customFormat = winston.format.printf(
-  ({ timestamp, level, message = "", ...meta }) =>
-    `[${timestamp}] ${level}\t ${message} ${formatMeta(meta)}`
-);
-
-// Todo: log into a file on production builds
-// logger.add(new winston.transports.Console({ format: winston.format.json() }))
-logger.add(
-  new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize({
-        level: true,
-      }),
-      winston.format.timestamp({
-        format: "YY-MM-DD HH:mm:SS",
-      }),
-      customFormat
-    ),
-  })
-);
 export default logger;
