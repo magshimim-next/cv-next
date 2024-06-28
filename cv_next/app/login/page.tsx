cd5cdd8d64bb77b2e5eae535b28a246e9ec3af2f
Arad Donenfeld <aradon267@gmail.com>
2024-06-28 18:38:06 +0300

    UPDATED: login button doesnt use supabase ui

--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -1,23 +1,34 @@
 "use client";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import Definitions from "@/lib/definitions";
-import { useTheme } from "next-themes";
-import { createClientComponent } from "@/helpers/supabaseBrowserHelper";
 
+import Definitions from "@/lib/definitions";
+import { createClientComponent } from "@/helpers/supabaseBrowserHelper";
+import { FcGoogle } from "react-icons/fc";
+import { Button } from "@/components/ui/button";
 export default function Page() {
-  const { theme } = useTheme();
   const supabase = createClientComponent();
+
+  const signIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo:
+          process.env.NEXT_PUBLIC_BASE_URL + Definitions.AUTH_CALLBACK_REDIRECT,
+      },
+    });
+  };
+
   return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      theme={theme}
-      providers={["google"]}
-      redirectTo={
-        process.env.NEXT_PUBLIC_BASE_URL + Definitions.AUTH_CALLBACK_REDIRECT
-      }
-      onlyThirdPartyProviders
-    />
+    <main>
+      <div className="place-items-center px-4 text-sm font-medium">
+        <div className="p-4 md:p-5 lg:p-6">
+          <div className="grid gap-y-3">
+            <Button className="c-bOcPnF" onClick={signIn}>
+              <FcGoogle />
+              <span style={{ marginLeft: "5px" }}>Sign in with Google</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
