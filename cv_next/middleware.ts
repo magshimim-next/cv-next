24aa1d8c569a6da2756c60f5c6f30493f371e2b4
Arad Donenfeld <aradon267@gmail.com>
2024-06-28 22:57:33 +0300

    UPDATED: verifying session using JWT instead of getUser

--- cv_next/middleware.ts
+++ cv_next/middleware.ts
@@ -1,6 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
@@ -53,10 +54,36 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  const { data: activatedUser, error } = await supabase.auth.getUser();
+  if (!process.env.JWT_SECRET) {
+    // eslint-disable-next-line no-console
+    console.error(
+      "Error getting JWT, please configure your env file! \n \
+      Falling back to use getUser."
+    );
+    const { data: activatedUser, error } = await supabase.auth.getUser();
+    if (error || !activatedUser?.user) {
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
+  } else {
+    const { data: activatedUser, error } = await supabase.auth.getSession();
+    if (error || !activatedUser.session?.user) {
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
 
-  if (error || !activatedUser?.user) {
-    return NextResponse.rewrite(new URL("/login", request.url));
+    const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
+    try {
+      const { payload } = await jwtVerify(
+        activatedUser.session.access_token,
+        secretKey
+      );
+      if (payload.sub != activatedUser.session.user.id) {
+        // TODO: Someone deliberatly changed the token, maybe we should make note of that somehow
+        return NextResponse.rewrite(new URL("/login", request.url));
+      }
+    } catch (error) {
+      // TODO: Someone deliberatly changed the token, maybe we should make note of that somehow
+      return NextResponse.rewrite(new URL("/login", request.url));
+    }
   }
 
   return response;
