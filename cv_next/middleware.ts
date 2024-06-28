e11c7e96b61bfd5219edfb0517d045dd479c2ef5
Arad Donenfeld <aradon267@gmail.com>
2024-06-28 16:54:56 +0300

    UPDATED: response creation supabase middleware

--- cv_next/middleware.ts
+++ cv_next/middleware.ts
@@ -4,9 +4,7 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
+    request,
   });
 
   const supabase = createServerClient(
