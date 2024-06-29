ddfa4eae7d5be19bbcaaca76cda0432f504c4aa5
Arad Donenfeld <aradon267@gmail.com>
2024-06-29 11:03:59 +0300

    UPDATED: CVItem now showing all categories

--- cv_next/app/feed/components/CVItem.tsx
+++ cv_next/app/feed/components/CVItem.tsx
@@ -24,9 +24,8 @@ export default function CVItem({ cv }: CVCardProps) {
     Definitions.PLAICEHOLDER_IMAGE_DATA
   );
 
-  const categoryLink = `/feed?category=${Categories.category[
-    cv.category_id
-  ].toLowerCase()}`;
+  const generateCategoryLink = (categoryNumber: number) =>
+    `/feed?category=${Categories.category[categoryNumber].toLowerCase()}`;
 
   const getBlur = useMemo(
     () => async (imageURL: string) => {
@@ -116,19 +115,25 @@ export default function CVItem({ cv }: CVCardProps) {
       <div className="overlay gradient-blur-backdrop absolute bottom-0 flex h-full w-full rounded-lg backdrop-blur-[0.5px] transition hover:via-transparent hover:backdrop-blur-none">
         <div className="overlay absolute bottom-0 h-1/5 w-full rounded-xl bg-transparent p-6">
           <div className="absolute bottom-0 left-0 mx-5 mb-2.5">
-            <div className="mr-2 inline text-xl font-bold text-neutral-700">
-              {authorName}
+            <div className="flex flex-wrap items-baseline">
+              <div className="mr-2 text-xl font-bold text-neutral-700">
+                {authorName}
+              </div>
+              <p className="text-xs text-neutral-400">{formattedDate}</p>
             </div>
-            <p className="inline text-xs text-neutral-400">{formattedDate}</p>
+            <div className="mt-2 flex flex-wrap space-x-2">
+              {cv.cv_categories.map((category, index) => (
+                <Link key={index} href={generateCategoryLink(category)}>
+                  <div
+                    onClick={(e) => e.stopPropagation()} // Prevent redirection and handle the inner click event
+                    className="rounded-full bg-gray-700 px-3 py-1 text-sm font-semibold text-white hover:bg-gray-400 hover:underline"
+                  >
+                    #{Categories.category[category]}
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
-          <Link href={categoryLink}>
-            <div
-              onClick={(e) => e.stopPropagation()} // Add the onClick event handler to prevent redirection and handle the inner click event
-              className="absolute bottom-0 right-0 mx-4 mb-2.5 rounded-full bg-gray-700 px-3 py-1 text-sm font-semibold text-white hover:bg-gray-400 hover:underline"
-            >
-              #{Categories.category[cv.category_id]}
-            </div>
-          </Link>
         </div>
       </div>
     </>
