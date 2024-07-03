220d2e08a79ffa7cade8c39a67175da538d2eb78
Arad Donenfeld <aradon267@gmail.com>
2024-07-03 17:55:15 +0300

    ADDED: upload CV server action

--- cv_next/app/actions/cvs/uploadCv.ts
+++ cv_next/app/actions/cvs/uploadCv.ts
@@ -3,28 +3,46 @@
 import { InputValues } from "@/app/upload/page";
 import { uploadCV, getCvsByUserId } from "@/server/api/cvs";
 import SupabaseHelper from "@/server/api/supabaseHelper";
+import { PostgrestError } from "@supabase/supabase-js";
+import MyLogger from "@/server/base/logger";
+import { validateGoogleViewOnlyUrl } from "@/helpers/cvLinkRegexHelper";
 
 export const checkUploadCV = async ({
   cvData,
+  userId,
 }: {
   cvData: InputValues;
-}): Promise<Error | PostgrestError | null> => {
-  const userId = (await SupabaseHelper.getSupabaseInstance().auth.getUser())
-    .data.user?.id;
-  if (!userId) {
-    return Error("Couldn't get current user's ID");
-  }
+  userId: string;
+}): Promise<Error | void> => {
+  // const userId = (await SupabaseHelper.getSupabaseInstance().auth.getUser())
+  //   .data?.user?.id;
+  // if (!userId) {
+  //   MyLogger.logInfo("Couldn't get current user's ID");
+  //   return Error("Couldn't get current user's ID");
+  // }
   const cvsOfUser = await getCvsByUserId(userId);
   if (!cvsOfUser || cvsOfUser.length >= 5) {
+    MyLogger.logInfo("The user has at least 5 CVs already");
     return Error("The user has at least 5 CVs already");
   }
 
-  const cvToUpload: CvModel = {
+  if (!validateGoogleViewOnlyUrl(cvData.link)) {
+    MyLogger.logInfo("Regex invalid!", cvData.link);
+    return Error("Regex invalid!");
+  }
+
+  const cvToUpload: NewCvModel = {
     document_link: cvData.link,
     description: cvData.description,
     category_id: cvData.catagoryId,
     user_id: userId,
+    cv_categories: [cvData.catagoryId],
   };
 
-  const response = uploadCV();
+  MyLogger.logInfo("Can upload:", cvToUpload);
+
+  const response = await uploadCV(cvToUpload);
+  if (response) {
+    MyLogger.logInfo(response.message);
+  }
 };
