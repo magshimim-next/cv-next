0e5cd68a9d3c4c37465fd9639bf1032f0b90e1a5
Arad Donenfeld <aradon267@gmail.com>
2024-07-05 21:04:57 +0300

    UPDATED: lossened regex for view preview and edit

--- cv_next/helpers/cvLinkRegexHelper.ts
+++ cv_next/helpers/cvLinkRegexHelper.ts
@@ -1,5 +1,5 @@
 export const validateGoogleViewOnlyUrl = (url: string): boolean => {
   const pattern =
-    /https?:\/\/(?:docs|drive)\.google\.com\/(?:document|file)\/d\/([a-zA-Z0-9_-]+)\/(?:view|preview)(?:\?(?:usp=[a-zA-Z0-9_-]+&?)*)?/;
+    /https?:\/\/(?:docs|drive)\.google\.com\/(?:document|file)\/d\/([a-zA-Z0-9_-]+)\/(?:view|preview|edit)(?:\?(?:usp=[a-zA-Z0-9_-]+&?)*)?/;
   return pattern.test(url);
 };
