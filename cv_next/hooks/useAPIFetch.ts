114c348b6b8acc079a963b5d6ae82620d1cd0da5
Arad Donenfeld <aradon267@gmail.com>
2024-07-07 20:47:51 +0300

    ADDED: a generalized fetch hook

---
+++ cv_next/hooks/useAPIFetch.ts
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+
+export const CVS_API_BASE = "/api/cvs";
+
+export const getRouteUri = (base: string, endpoint: string): string => {
+  return `${base}/${endpoint}`;
+};
+
+export const useApiFetch = () => {
+  const fetchFromApi = useCallback(
+    async (base: string, endpoint: string, body: Record<string, any>) => {
+      const response = await fetch(getRouteUri(base, endpoint), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from ${endpoint}`);
+      }
+
+      const data = await response.json();
+      return data;
+    },
+    []
+  );
+
+  return fetchFromApi;
+};
