e1635e37e9df407adf3a6facfc719ffcf72b7cad
Arad Donenfeld <aradon267@gmail.com>
2024-07-23 19:00:42 +0300

    DELETED: handletype func

--- cv_next/app/actions/user/fetchUserInfo.ts
+++ cv_next/app/actions/user/fetchUserInfo.ts
@@ -7,37 +7,6 @@ import { redirect } from "next/navigation";
 import Definitions from "@/lib/definitions";
 
 /**
- * Handle which user can enter a given route(used instead of the middleware to reduce server requests)
- * If there's no user connect, return login and redirect there
- * If the user is inactive, return inactive and redirect there
- * If the user is active, return the given finalRedirect and redirect there
- * @param {string} finalRedirect - The path the user wants to access
- * @return {Promise<string>} a promised string with where to redirect the user to
- */
-export const handleCurrentUser = async (
-  finalRedirect: string
-): Promise<string> => {
-  const supabase = SupabaseHelper.getSupabaseInstance();
-  const { data: activatedUser, error } = await supabase.auth.getUser();
-  if (error || !activatedUser?.user) {
-    if (error) logger.error(error, "Error getting current user");
-    return "/login";
-  } else {
-    const { data: whitelisted, error } = await supabase
-      .from(Tables.whitelisted)
-      .select("*")
-      .eq(ProfileKeys.id, activatedUser.user.id)
-      .single();
-
-    if (whitelisted?.id == null || error) {
-      if (error) logger.error(error, "Error getting current user from DB");
-      return "/inactive";
-    }
-  }
-  return finalRedirect;
-};
-
-/**
  * Get the user based on a given ID
  * @param userId A string representing the user's ID
  * @returns UserModel of the user or null on error
