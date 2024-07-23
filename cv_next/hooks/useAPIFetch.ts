300301020fa9ac300219fde78b21acfcee1f0838
Arad Donenfeld <aradon267@gmail.com>
2024-07-23 20:14:19 +0300

    UPDATED: added swr and mutate for postt requests

--- cv_next/hooks/useAPIFetch.ts
+++ cv_next/hooks/useAPIFetch.ts
@@ -1,24 +1,40 @@
+import { mutate } from "swr";
 import { useCallback } from "react";
 
 export const getRouteUri = (base: string, endpoint: string): string => {
   return `${base}/${endpoint}`;
 };
 
+const postFetcher = async (url: string, body: Record<string, any>) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from ${url}`);
+  }
+
+  return response.json();
+};
+
+// Hook for POST requests
 export const useApiFetch = () => {
+  // Function to trigger a POST request
   const fetchFromApi = useCallback(
     async (base: string, endpoint: string, body: Record<string, any>) => {
-      const response = await fetch(getRouteUri(base, endpoint), {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-      });
+      const apiUrl = getRouteUri(base, endpoint);
 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data from ${endpoint}`);
+      // Call postFetcher directly
+      try {
+        const data = await postFetcher(apiUrl, body);
+        // Optionally update the cache
+        mutate(apiUrl, data, false);
+        return data;
+      } catch (error) {
+        throw error;
       }
-
-      const data = await response.json();
-      return data;
     },
     []
   );
