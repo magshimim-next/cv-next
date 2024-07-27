d3706a36dc411913c89907ef3da8f751f081473c
Arad Donenfeld <aradon267@gmail.com>
2024-07-27 16:28:06 +0300

    FIXED: encoding style

--- cv_next/app/auth/callback/route.ts
+++ cv_next/app/auth/callback/route.ts
@@ -13,6 +13,7 @@ export async function GET(request: Request) {
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
       {
+        cookieEncoding: "raw",
         cookies: {
           get(name: string) {
             return cookieStore.get(name)?.value;
--- cv_next/providers/supabase-provider.tsx
+++ cv_next/providers/supabase-provider.tsx
@@ -1,49 +1,48 @@
-"use client"
+"use client";
 
-import { createContext, useEffect } from "react"
+import { createContext, useEffect } from "react";
 import {
   Session,
   SupabaseClient,
   createClientComponentClient,
-} from "@supabase/auth-helpers-nextjs"
-import { useRouter } from "next/navigation"
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
-type MaybeSession = Session | null
+type MaybeSession = Session | null;
 
 type SupabaseContext = {
-  supabase: SupabaseClient<any, string>
-  session: MaybeSession
-}
+  supabase: SupabaseClient<any, string>;
+  session: MaybeSession;
+};
 
-export const Context = createContext<SupabaseContext | undefined>(undefined)
+export const Context = createContext<SupabaseContext | undefined>(undefined);
 
 export default function SupabaseProvider({
   children,
   session,
 }: {
-  children: React.ReactNode
-  session: MaybeSession
+  children: React.ReactNode;
+  session: MaybeSession;
 }) {
-  const supabase = createClientComponentClient()
-  const router = useRouter()
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
 
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_, _session) => {
       if (_session?.access_token !== session?.access_token) {
-        
       }
-    })
+    });
 
     return () => {
-      subscription.unsubscribe()
-    }
-  }, [router, supabase, session])
+      subscription.unsubscribe();
+    };
+  }, [router, supabase, session]);
 
   return (
     <Context.Provider value={{ supabase, session }}>
       <>{children}</>
     </Context.Provider>
-  )
+  );
 }
