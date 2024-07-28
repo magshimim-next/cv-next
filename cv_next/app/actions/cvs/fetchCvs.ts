4de917e8b0f4b3f072e0aa020b01bb9f9244b7be
Arad Donenfeld <aradon267@gmail.com>
2024-07-28 21:18:59 +0300

    UPDATED: getting all unique cvs first and then fetching objects

--- cv_next/app/actions/cvs/fetchCvs.ts
+++ cv_next/app/actions/cvs/fetchCvs.ts
@@ -5,19 +5,23 @@ import { getCvById } from "@/server/api/cvs";
 export const getCvsFromComments = async (
   comments: CommentModel[]
 ): Promise<CvModel[]> => {
-  const uniqueDocuments: Set<CvModel> = new Set(); // Use a Set to store unique documents
+  const uniqueCvs: Set<CvModel> = new Set(); // Use a Set to store unique documents
+  const uniqueCvsFromComments: Set<string> = new Set();
 
-  // Iterate through the comments
+  // Get unique cv ids
   for (const comment of comments) {
-    const document_id: string = comment.document_id;
-    if (document_id !== undefined) {
-      // Use the placeholder function to get the document by its ID
-      const document = await getCvById(document_id);
-      if (document) {
-        uniqueDocuments.add(document); // Add the document to the Set
-      }
+    if (comment.document_id !== undefined) {
+      uniqueCvsFromComments.add(comment.document_id);
     }
   }
 
-  return Array.from(uniqueDocuments); // Convert Set to array and return
+  // Get cvs set
+  for (const cvId of uniqueCvsFromComments) {
+    const cv = await getCvById(cvId);
+    if (cv) {
+      uniqueCvs.add(cv); // Add the document to the Set
+    }
+  }
+
+  return Array.from(uniqueCvs); // Convert Set to array and return
 };
