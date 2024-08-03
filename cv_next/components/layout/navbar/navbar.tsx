110669b428e67e1b2a78b945e5b3071c7d19e83b
Arad Donenfeld <aradon267@gmail.com>
2024-08-03 14:07:18 +0300

    UPDATED: style and clickoutside

--- cv_next/components/layout/navbar/navbar.tsx
+++ cv_next/components/layout/navbar/navbar.tsx
@@ -36,89 +36,95 @@ export default function Navbar() {
   };
 
   return (
-    <header className="fixed top-0 z-50 w-full select-none bg-black bg-opacity-75 backdrop-blur-2xl">
-      <nav className="mx-auto flex items-center justify-between px-4 py-3 md:px-8 lg:max-w-7xl">
-        <Link href="/">
-          <h1 className="text-2xl font-bold text-white duration-200 md:text-3xl lg:text-4xl lg:hover:scale-110">
-            CV-NEXT
-          </h1>
-        </Link>
-        <div className="flex items-center space-x-4 md:hidden">
-          <button
-            ref={buttonRef}
-            className="text-white focus:outline-none"
-            onClick={handleClick}
-            onBlur={handleBlur}
+    <>
+      <header className="relative top-0 h-[72px] w-full select-none">
+        <div className="fixed top-0 -z-50 h-[36px] w-full translate-y-2/4 bg-white" />
+        <div className="fixed top-0 z-40 h-[72px] w-full bg-black bg-opacity-75 backdrop-blur-2xl" />
+        <div className="fixed top-0 z-50 h-[72px] w-full">
+          <nav className="mx-auto flex items-center justify-between px-4 py-3 md:px-8 lg:max-w-7xl">
+            <Link href="/">
+              <h1 className="text-2xl font-bold text-white duration-200 lg:hover:scale-[1.10]">
+                CV-NEXT
+              </h1>
+            </Link>
+
+            <div className="hidden items-center space-x-6 md:flex">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.route}
+                  className="py-2 text-lg font-medium text-white hover:underline"
+                  href={link.path}
+                  style={{ lineHeight: "1.5" }}
+                >
+                  {link.route}
+                </Link>
+              ))}
+            </div>
+
+            <div className="flex items-center space-x-4">
+              <div className="hidden space-x-4 md:flex">
+                <ModeToggle />
+                <PopupToggle />
+              </div>
+              <button
+                ref={buttonRef}
+                className="text-white focus:outline-none md:hidden"
+                onClick={handleClick}
+                onBlur={handleBlur}
+              >
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  {isMenuOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  )}
+                </svg>
+              </button>
+            </div>
+          </nav>
+        </div>
+        {isMenuOpen && (
+          <div
+            ref={menuRef}
+            className="fixed left-0 right-0 top-[72px] bg-slate-500 bg-opacity-75 p-4 backdrop-blur-2xl dark:bg-black md:hidden"
+            tabIndex={-1}
           >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {isMenuOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
-        <div className="hidden flex-grow items-center justify-center md:flex">
-          <ul className="flex space-x-6">
-            {navLinks.map((link) => (
-              <li key={link.route}>
-                <Link
-                  className="text-lg font-medium text-white hover:underline md:text-xl lg:text-2xl"
-                  href={link.path}
-                >
-                  {link.route}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="hidden items-center space-x-5 md:flex">
-          <ModeToggle />
-          <PopupToggle />
-        </div>
-      </nav>
-      {isMenuOpen && (
-        <div
-          ref={menuRef}
-          className="bg-slate-500 bg-opacity-75 p-4 backdrop-blur-2xl dark:bg-black md:hidden"
-          tabIndex={-1}
-        >
-          <ul className="space-y-4">
-            {navLinks.map((link) => (
-              <li key={link.route}>
-                <Link
-                  className="text-lg font-medium text-white hover:underline"
-                  href={link.path}
-                  onClick={handleClick}
-                >
-                  {link.route}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className="mt-4 flex justify-between">
-            <ModeToggle />
-            <PopupToggle closeHamburger={handleClick} />
+            <ul className="space-y-4">
+              {navLinks.map((link) => (
+                <li key={link.route}>
+                  <Link
+                    className="text-lg font-medium text-white hover:underline"
+                    href={link.path}
+                    onClick={handleClick}
+                  >
+                    {link.route}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-4 flex justify-between">
+              <ModeToggle />
+              <PopupToggle closeHamburger={handleClick} />
+            </div>
           </div>
-        </div>
-      )}
-    </header>
+        )}
+      </header>
+    </>
   );
 }
