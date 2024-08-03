65648db4c97c468b4896741f1d0df97a807158f8
Arad Donenfeld <aradon267@gmail.com>
2024-08-03 22:01:06 +0300

    FIXED: signout

--- cv_next/lib/definitions.ts
+++ cv_next/lib/definitions.ts
@@ -1,7 +1,7 @@
 export const uiLocation = {
-  navbar: 'navbar',
-  profile: 'profile'
-} as const
+  navbar: "navbar",
+  profile: "profile",
+} as const;
 
 export default class Definitions {
   public static readonly undefinedIndex = -1;
@@ -29,39 +29,38 @@ export const heroHeader: PageHeader = {
   image: `/images/logo.png`,
   subheader: `Improve the Magshimim Next Community's resumes.`,
   explanation: `As a community, we can help each other secure jobs by enhancing our members' resumes. This website is a platform to upload resumes, allow others to review them, and make them better.
-We expect all interactions to be respectful and constructive, with no tolerance for abuse, hateful comments, or any form of inappropriate behavior.`
-}
+We expect all interactions to be respectful and constructive, with no tolerance for abuse, hateful comments, or any form of inappropriate behavior.`,
+};
 
 export const inactiveHeader: PageHeader = {
   header: `Access Denied`,
   subheader: `This page requieres that you get approved by the moderators`,
   image: `/access-denied.webp`,
-}
-
+};
 
 export const routes: routes[] = [
   {
     route: "Home",
     path: "/",
-    image: '/images/home.png',
-    UILocation: uiLocation.navbar
+    image: "/images/home.png",
+    UILocation: uiLocation.navbar,
   },
   {
     route: "Feed",
     path: "/feed",
-    image: '/images/feed.png',
-    UILocation: uiLocation.navbar
+    image: "/images/feed.png",
+    UILocation: uiLocation.navbar,
   },
   {
     route: "Login",
     path: "/login",
-    image: '',
-    UILocation: uiLocation.profile
+    image: "",
+    UILocation: uiLocation.profile,
   },
   {
     route: "Signout",
-    path: "/",
-    image: '',
-    UILocation: uiLocation.profile
+    path: "/signout",
+    image: "",
+    UILocation: uiLocation.profile,
   },
-]
+];
