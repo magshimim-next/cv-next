3539dc8cca94f787cd5f2381e5e61484e8bb01fb
Arad Donenfeld <aradon267@gmail.com>
2024-08-05 22:30:41 +0300

    FIXED: signout caused error

--- cv_next/app/signout/page.tsx
+++ cv_next/app/signout/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 import { createClientComponent } from "@/helpers/supabaseBrowserHelper";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
   const router = useRouter();
   const supabase = createClientComponent();
-  supabase.auth.signOut();
-  router.push("/");
+  useEffect(() => {
+    const signOutAndRedirect = async () => {
+      await supabase.auth.signOut();
+      router.refresh();
+      router.push("/");
+    };
+
+    signOutAndRedirect();
+  }, [router, supabase]);
+
   return <></>;
 }
