04d55d4a0d115f815c09c9a4dc4b4785971448c3
Arad Donenfeld <aradon267@gmail.com>
2024-08-07 03:29:33 +0300

    FIXED: redirect URL

--- cv_next/app/login/components/login.tsx
+++ cv_next/app/login/components/login.tsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation";
 
 export default function Login() {
   const searchparams = useSearchParams();
-  const next = searchparams.get("next") || "/feed";
+  const next = searchparams.get("next") || "feed";
   return (
     <div className="place-items-center px-4 text-sm font-medium">
       <div className="p-4 md:p-5 lg:p-6">
