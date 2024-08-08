26fc0d1d6f7cf37a2cdbf833d40c62516320f682
YechiamWe <yechiamwe@gmail.com>
2024-08-08 23:35:35 +0300

    ADDED: useUser hook

---
+++ cv_next/hooks/useUser.ts
@@ -0,0 +1,19 @@
+import { useSupabase } from "./supabase";
+import useSWR from "swr";
+
+
+export const useUser = () => {
+    const supabase = useSupabase();
+    const { data, mutate, error } = useSWR('user', supabase.auth.getUser);
+
+    const loading = !data && !error;
+    const loginState = data && !error;
+    //const loggedOut = error && error.status === 403; : might be useful instead of loginState
+
+    return {
+        loading,
+        loginState,
+        user: data,
+        mutateUser: mutate
+    };
+}
