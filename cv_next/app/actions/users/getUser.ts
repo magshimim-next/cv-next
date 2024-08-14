6d0b109d73432a145c87ae4395860e80bfdcb38e
YechiamWe <yechiamwe@gmail.com>
2024-08-14 20:42:24 +0300

    UPDATE: adjust hook to work with server actions, add user data to the hook, and make sure no unneccesary revalidations takes place

--- cv_next/app/actions/users/getUser.ts
+++ cv_next/app/actions/users/getUser.ts
@@ -1,5 +1,4 @@
 "use server";
-"use server";
 
 import { getUserById } from "@/server/api/users";
 import { Err } from "@/lib/utils";
@@ -7,6 +6,7 @@ import { logErrorWithTrace } from "@/server/base/logger";
 import SupabaseHelper from "@/server/api/supabaseHelper";
 import { notFound } from "next/navigation";
 import { ProfileKeys } from "@/lib/supabase-definitions";
+import { User } from "@supabase/supabase-js";
 
 /**
  * Retrieves user data by user ID.
@@ -25,6 +25,41 @@ export const getUserModel = async (
     return Err(
       "An error has occurred while fetching the user data. Please try again later."
     );
+  }
+};
+
+//expose auth.getUser to client
+type UserSessionResponse =
+  | {
+      data: {
+        user: User
+      }
+      error: null
+    }
+  | {
+      data: {
+        user: undefined
+      }
+      error: {
+        message: string,
+        status: number | undefined
+      }
+    };
+export const getUserSession = async ():Promise<UserSessionResponse> => {
+  const supabase = SupabaseHelper.getSupabaseInstance();
+  const userSession = await supabase.auth.getUser();
+  if (!userSession.error) {
+    return userSession;
+  } else {
+    return {
+      data: {
+        user: undefined
+      },
+      error: {
+        message: userSession.error.message,
+        status: userSession.error.status
+      }
+    };
   }
 };
 
--- cv_next/hooks/useUser.ts
+++ cv_next/hooks/useUser.ts
@@ -1,21 +1,53 @@
 "use client";
 
-import { useSupabase } from "./supabase";
+import { getUserModel, getUserSession } from "@/app/actions/users/getUser";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+const getUserClient = async () => {
+    const session = await getUserSession();
+    const userSession = session?.data?.user;
+    let userData;
+    if (userSession) {
+        const dataFetch = await getUserModel(userSession.id);
+        if (dataFetch.ok) {
+            userData = dataFetch.val;
+        }
+    }
+    return {
+        session,
+        userData
+    }
+}
 
 export const useUser = () => {
-    const supabase = useSupabase();
-    const { data, mutate, error } = useSWR('user', supabase.auth.getUser);
+    const [ mounted, setMounted ] = useState(false);
 
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    const { data, mutate, error } = useSWR('user', getUserClient, {
+        revalidateOnMount: false,
+        revalidateOnFocus: false
+    });
+
+    const activeUser = data?.session?.data?.user;
     const loading = !data && !error;
-    const loginState = data && !error;
+    const loginState = activeUser && !error;
     //const loggedOut = error && error.status === 403; : might be useful instead of loginState
 
+    useEffect(() => {
+        if (mounted && !data && !error) {
+          mutate();
+        }
+      }, [mounted, data, error, mutate]);
+      
     return {
         loading,
         loginState,
-        user: data,
+        userSession: activeUser || undefined,
+        userData: data?.userData || undefined,
         mutateUser: mutate
     };
 }
