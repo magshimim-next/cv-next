eee40569a3969be5bf4296915baed1aa81f3ec7d
Arad Donenfeld <aradon267@gmail.com>
2024-08-23 20:26:50 +0300

    UPDATED: deleteComment seperated functions

--- cv_next/app/actions/comments/deleteComment.ts
+++ cv_next/app/actions/comments/deleteComment.ts
@@ -6,14 +6,13 @@ import { getCurrentId, userIsAdmin } from "@/server/api/users";
 import logger, { logErrorWithTrace } from "@/server/base/logger";
 
 /**
- * Deletes a comment by its ID.
- *
- * @param {string} commentId - The ID of the comment to be deleted
- * @return {Promise<Result<void, string>>} A Promise that resolves to a Result object containing no value if successful, or an error message
+ * The function will see if the comment can be deleted
+ * @param commentResult The comment model to be deleted
+ * @returns A promise of error or null based on if the action can be performed
  */
-export const deleteComment = async (
-  commentId: string
-): Promise<Result<void, string>> => {
+async function validateDelete(
+  commentResult: Result<CommentModel, string>
+): Promise<Result<never, string> | null> {
   const currentIdResult = await getCurrentId();
   if (!currentIdResult.ok) {
     logErrorWithTrace(currentIdResult);
@@ -21,7 +20,7 @@ export const deleteComment = async (
       "An error has occurred while deleting the comment. Please try again later."
     );
   }
-  const commentResult = await getCommentById(commentId);
+
   if (!commentResult.ok) {
     logErrorWithTrace(commentResult);
     return Err(
@@ -40,6 +39,31 @@ export const deleteComment = async (
       "An error has occurred while deleting the comment. Please try again later."
     );
   }
+  return null;
+}
+
+/**
+ * Deletes a comment by its ID.
+ *
+ * @param {string} commentId - The ID of the comment to be deleted
+ * @return {Promise<Result<void, string>>} A Promise that resolves to a Result object containing no value if successful, or an error message
+ */
+export const deleteComment = async (
+  commentId: string
+): Promise<Result<void, string>> => {
+  const commentResult = await getCommentById(commentId);
+  if (!commentResult.ok) {
+    logErrorWithTrace(commentResult);
+    return Err(
+      "An error has occurred while deleting the comment. Please try again later."
+    );
+  }
+
+  const validateError = await validateDelete(commentResult);
+  if (validateError) {
+    return validateError;
+  }
+
   const result = await markCommentAsDeleted(commentId);
   if (result.ok) {
     return result;
