994b2a7d52c5a4991413b21bf405552ac0f3e42a
Arad Donenfeld <aradon267@gmail.com>
2024-08-23 12:41:45 +0300

    UPDATED: return 500 on non allowed paths redirect

--- cv_next/app/auth/callback/route.ts
+++ cv_next/app/auth/callback/route.ts
@@ -2,19 +2,27 @@
 import { NextResponse } from "next/server";
 import Definitions from "@/lib/definitions";
 import SupabaseHelper from "@/server/api/supabaseHelper";
+import { checkRedirect } from "@/lib/utils";
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next");
+  const next = searchParams.get("next") || "";
 
   if (code) {
     const supabase = SupabaseHelper.getSupabaseInstance();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(
-        `${origin}` + Definitions.AUTH_DEFAULT_REDIRECT + next
-      );
+      if (checkRedirect(next)) {
+        return NextResponse.redirect(
+          `${origin}${Definitions.AUTH_DEFAULT_REDIRECT}${next}`
+        );
+      } else {
+        return NextResponse.json(
+          { error: "Failed to exchange code for session with Supabase." },
+          { status: 500 }
+        );
+      }
     }
   }
 
