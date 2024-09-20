2c36f6b964190ede0846c1fb8947d489f317a7ef
Arad Donenfeld <aradon267@gmail.com>
2024-09-20 21:19:46 +0300

    UPDATED: checking user state before rendering landing page

--- cv_next/app/(landing)/page.tsx
+++ cv_next/app/(landing)/page.tsx
@@ -1,7 +1,18 @@
-import { Suspense } from "react";
+"use client";
+import { Suspense, useEffect } from "react";
 import Feed from "@/app/feed/components/feed";
+import { useUser } from "@/hooks/useUser";
+import { AboutLayout } from "@/app/about/components/aboutLayout";
 
-export default async function Page() {
+export default function Page() {
+  const { loading, loginState, mutateUser } = useUser();
+
+  useEffect(() => {
+    mutateUser();
+  }, [mutateUser]);
+
+  if (loading || !loginState) return <AboutLayout />;
+
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
