1640f998a06c3df10ed38f9930815ef87cfac6e3
TheGreenOak <alongreenalgr@gmail.com>
2024-10-09 23:49:43 +0300

    added a folder for icons
    and started using it

--- cv_next/components/ui/pageFooter.tsx
+++ cv_next/components/ui/pageFooter.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Credits } from "@/lib/definitions";
+import Image from 'next/image';
+import linkedin from '@/public/icons/linkedin.svg';
 
 
 const Footer: React.FC = () => {
@@ -14,7 +16,7 @@ const Footer: React.FC = () => {
                 <a href="https://www.linkedin.com" target="_blank" className='max-w-24 max-h-24 mx-3 pr-4 py-5 dark:fill-slate-100'>
                     {/* <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg> */}
                 {/* <svg className='max-w-24 max-h-24 mx-3 pr-4 dark:fill-slate-100' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg> */}
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="47" height="47" viewBox="0 0 50 50"><path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path></svg>
+                    <Image src={linkedin} alt=''></Image>
                 </a>
                 <hr className ="w-11/12 h-1 mx-auto my-4 border-0 rounded md:my-10 dark:bg-slate-500"/>
             </div>
