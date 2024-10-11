d745159a1e7084abe59b4c60b8a45619bd5bb033
TheGreenOak <alongreenalgr@gmail.com>
2024-10-12 00:46:31 +0300

    made a cooler 404 page
    yeah just one commit deal with it

--- cv_next/app/not-found.tsx
+++ cv_next/app/not-found.tsx
@@ -1,23 +1,41 @@
 import Image from "next/image";
 import notFound404 from "../public/images/404.png";
 import notFound404Light from "../public/images/404Light.png";
+import chair from "../public/images/chair.png";
 
 export default function NotFound() {
   return (
+    <div>
     <div className="left-0 flex h-3/4 w-full items-center justify-center space-y-8 p-6">
       <div className="flex flex-row items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-row items-center-justify-center">
+            <div>
+              <Image
+                alt="closeIcon"
+                src={notFound404}
+                className="dark:hidden"
+              ></Image>
+              <Image
+                alt="closeIcon"
+                src={notFound404Light}
+                className="hidden dark:block"
+              ></Image>
+            </div>
+            <h1 className="pl-10 text-7xl text-left place-self-center"> Page Not Found</h1>
+          </div>
+          <div className="flex flex-row items-center-justify-center">
+            <h2 className="text-3xl text-left place-self-center">This page didn’t make it past the recruiter. Try another page!&nbsp;<a href="/" className="underline">Go Home</a></h2>
+          </div>
+        </div>
         <Image
-          alt="closeIcon"
-          src={notFound404}
-          className="dark:hidden"
-        ></Image>
-        <Image
-          alt="closeIcon"
-          src={notFound404Light}
-          className="hidden dark:block"
+          alt="chair"
+          src={chair}
+          className="w-1/2"
         ></Image>
       </div>
-      <h1 className="pl-10 text-7xl"> Page Not Found</h1>
     </div>
+    </div>
+    
   );
 }
