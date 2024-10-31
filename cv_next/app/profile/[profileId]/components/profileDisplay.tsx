dff6d5e123dac6df2ef394f4a3de58fb2a27caa3
YechiamWe <yechiamwe@gmail.com>
2024-10-31 13:47:36 +0200

    UPDATE: change editing in profile page to use react-hook-form, and related changes

---
+++ cv_next/app/profile/[profileId]/components/profileDisplay.tsx
@@ -0,0 +1,26 @@
+import Categories from "@/types/models/categories";
+
+export const ProfileDisplay = ({ user }: { user: UserModel }) => {
+  return (
+    <>
+      <div className="m-auto w-full">
+        <div className="flex justify-between">
+          <label htmlFor="username">Username: </label>
+          <span id="username">{user.username}</span>
+        </div>
+        <div className="flex justify-between">
+          <label htmlFor="workCategories">Work Categories: </label>
+          <span id="workCategories">
+            {user.work_status_categories
+              ?.map((id) => Categories.category[id])
+              .join(", ") || "None"}
+          </span>
+        </div>
+        <div className="flex justify-between">
+          <label htmlFor="workStatus">Work Status: </label>
+          <span id="workStatus">{user.work_status || "Nothing"}</span>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ cv_next/app/profile/[profileId]/components/profileForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { useMemo, useTransition } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useUser } from "@/hooks/useUser";
+import Categories from "@/types/models/categories";
+import { MultiSelect } from "@/components/ui/multiSelect";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { updateUserAction } from "@/app/actions/users/updateUser";
+import { ProfileKeys } from "@/lib/supabase-definitions";
+
+type FormValues = {
+  username: string;
+  workCategories: number[];
+  workStatus: keyof typeof ProfileKeys.work_status;
+};
+
+export default function ProfileForm({
+  user,
+  revalidationFn,
+  exitEditMode,
+}: {
+  user: UserModel;
+  revalidationFn: () => void;
+  exitEditMode: () => void;
+}) {
+  const {
+    register,
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: "onChange" });
+  const { userData, mutateUser } = useUser();
+  const [isPending, startTransition] = useTransition();
+
+  const mapCategories: number[] = useMemo(() => {
+    const keys = Object.keys(Categories.category)
+      .map((key) => parseInt(key))
+      .filter((key) => !isNaN(key));
+    return keys;
+  }, []);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  const handleOnSubmit: SubmitHandler<FormValues> = async (data) => {
+    const { username, workCategories, workStatus } = data;
+    let userDataToUpdate: Partial<UserModel> = {
+      id: user.id,
+    };
+    // Only update if the value has changed
+    if (username !== user.username) {
+      //TODO: username needs to be handled very differently!
+      userDataToUpdate.username = username;
+    }
+    if (
+      JSON.stringify(workCategories.toSorted()) !==
+      JSON.stringify(user.work_status_categories?.toSorted())
+    ) {
+      userDataToUpdate.work_status_categories = workCategories;
+    }
+    if (workStatus !== user.work_status) {
+      userDataToUpdate.work_status = workStatus;
+    }
+
+    if (Object.keys(userDataToUpdate).length === 1) {
+      // No changes
+      exitEditMode();
+      return;
+    }
+    startTransition(async () => {
+      const result = await updateUserAction(userDataToUpdate);
+      if (result.ok) {
+        // Revalidate cache for user
+        revalidationFn();
+        await mutateUser();
+        exitEditMode();
+      } else {
+        const error =
+          result.errors?.postgrestError ??
+          result.errors?.authError ??
+          result.errors?.err;
+        setError("root", { message: error?.message ?? "An error occurred" });
+      }
+    });
+  };
+
+  const categoriesValidation = {
+    //NOTE: after discussion, this validation might be removed.
+    validate: {
+      maxSize: (selectedCategories: number[]) =>
+        selectedCategories.length <= 3 || "Max 3 categories",
+    },
+  };
+
+  return (
+    <form onSubmit={handleSubmit(handleOnSubmit)}>
+      <div className="flex justify-between">
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          {...register("username", { required: "Username is required" })}
+          defaultValue={user.username ?? ""}
+        />
+        {errors.username && (
+          <FormErrorMessage message={errors.username.message} />
+        )}
+      </div>
+
+      <MultiSelect<FormValues>
+        name="workCategories"
+        label="Work Categories: "
+        options={mapCategories}
+        labels={mapCategories.map((id) => Categories.category[id])}
+        control={control}
+        defaultValue={user.work_status_categories ?? undefined}
+        validation={categoriesValidation}
+      />
+
+      <div className="flex justify-between">
+        <label htmlFor="workStatus">Work Status</label>
+        <select
+          id="workStatus"
+          {...register("workStatus", { required: "Work status is required" })}
+          defaultValue={user.work_status ?? "nothing"}
+        >
+          {Object.entries(ProfileKeys.work_status).map(([key, value]) => (
+            <option key={key} value={key}>
+              {value}
+            </option>
+          ))}
+        </select>
+        {errors.workStatus && (
+          <FormErrorMessage message={errors.workStatus.message} />
+        )}
+      </div>
+
+      <div className="mt-2 flex justify-end">
+        <button
+          className={`${cn(buttonVariants({ size: "sm" }))}`}
+          type="submit"
+          disabled={isPending || !!Object.keys(errors).length}
+        >
+          {isPending ? "Updating..." : "Update Profile"}
+        </button>
+      </div>
+      {errors.root && <FormErrorMessage message={errors.root.message} />}
+    </form>
+  );
+}
+
+const FormErrorMessage = ({ message }: { message?: string }) => (
+  <div className="mb-2 text-red-500">{message}</div>
+);
