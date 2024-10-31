3e1c3d53f5a19703e772969d0ea41e1fa27ac599
lordYorden <87636389+lordYorden@users.noreply.github.com>
2024-10-31 18:56:05 +0200

    fixed to use tailwind instead of style tag

--- cv_next/components/ui/tooltip.tsx
+++ cv_next/components/ui/tooltip.tsx
@@ -27,8 +27,7 @@ export default function Tooltip({
           role="tooltip"
           className={`tooltip absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white shadow-sm transition-opacity duration-300 dark:bg-gray-700 ${
             visible ? "opacity-100" : "opacity-0"
-          }`}
-          style={{ left: "50%", transform: "translateX(-50%)" }}
+          } left-1/2 -translate-x-1/2 transform`}
         >
           {message}
           <div className="tooltip-arrow" data-popper-arrow></div>
