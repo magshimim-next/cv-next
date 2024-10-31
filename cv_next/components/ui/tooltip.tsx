017dd06958055cca923621d6d6e184fb3a7b7355
lordYorden <87636389+lordYorden@users.noreply.github.com>
2024-10-31 18:44:25 +0200

    fixed tooltip to be centered around children

--- cv_next/components/ui/tooltip.tsx
+++ cv_next/components/ui/tooltip.tsx
@@ -14,16 +14,21 @@ export default function Tooltip({
 
   return (
     <>
-      <div>
-        {children}
+      <div className="relative inline-block">
+        <div
+          onMouseEnter={() => setVisible(true)}
+          onMouseLeave={() => setVisible(false)}
+        >
+          {children}
+        </div>
+
         <div
           id={id}
           role="tooltip"
           className={`tooltip absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white shadow-sm transition-opacity duration-300 dark:bg-gray-700 ${
             visible ? "opacity-100" : "opacity-0"
           }`}
-          onMouseEnter={() => setVisible(true)}
-          onMouseLeave={() => setVisible(false)}
+          style={{ left: "50%", transform: "translateX(-50%)" }}
         >
           {message}
           <div className="tooltip-arrow" data-popper-arrow></div>
