707bfacf1bc8e48e18732ccdf4b28102c034668f
lordYorden <87636389+lordYorden@users.noreply.github.com>
2024-11-01 15:58:25 +0200

    changed detection method for sign in

--- cv_next/app/profile/[profileId]/components/firstTimeSignIn.tsx
+++ cv_next/app/profile/[profileId]/components/firstTimeSignIn.tsx
@@ -3,46 +3,40 @@ import React, { useEffect, useState } from "react";
 import { FaQuestionCircle } from "react-icons/fa";
 import { createClientComponent } from "@/helpers/supabaseBrowserHelper";
 import Tooltip from "@/components/ui/tooltip";
+import { getUser } from "@/app/actions/users/getUser";
 
 export default function FirstTimeSignIn() {
   const [isFirstLogin, setIsFirstLogin] = useState<boolean>(false);
-  //const [isHovering, setIsHovering] = useState<boolean>(false);
   const superbase = createClientComponent();
 
   useEffect(() => {
     async function checkFirstLogin() {
-      const user = (await superbase.auth.getUser()).data.user;
-      if (!user) return; //TODO: maybe push to inactive page
-      setIsFirstLogin(user.last_sign_in_at != user.created_at); //TODO: change to == (!= for debuging)
+      const user = await getUser();
+      if (!user.ok) return; //TODO: maybe push to inactive page
+
+      const username = user.val.username;
+      setIsFirstLogin(
+        username !== undefined && username !== null && username.includes(".")
+      );
+
+      // const user = (await superbase.auth.getUser()).data.user;
+      // if (!user) return; //TODO: maybe push to inactive page
+      // setIsFirstLogin(user.last_sign_in_at != user.created_at); //TODO: change to == (!= for debuging)
     }
 
     checkFirstLogin();
   }, [superbase.auth]);
 
-  // const onHover = () => {
-  //     setIsHovering(true);
-  // };
-
-  // const onLeave = () => {
-  //     setIsHovering(false);
-  // };
-
   return (
     <>
-      {
-        isFirstLogin && (
-          // <div
-          // onMouseEnter={onHover}
-          // onMouseLeave={onLeave}>
-          <Tooltip
-            message="First time sign in? Change your username"
-            id="firstTime"
-          >
-            <FaQuestionCircle />
-          </Tooltip>
-        )
-        //</div>
-      }
+      {isFirstLogin && (
+        <Tooltip
+          message="First time sign in? Change your username"
+          id="firstTime"
+        >
+          <FaQuestionCircle />
+        </Tooltip>
+      )}
     </>
   );
 }
