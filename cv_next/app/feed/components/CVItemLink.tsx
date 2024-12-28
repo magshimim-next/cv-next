ff70f379eb05c286d8ea7543a19367ee0b261296
Arad Donenfeld <aradon267@gmail.com>
2024-12-28 14:59:34 +0200

    FIXED: cv stretches beyond borders on smaller devices

--- cv_next/app/feed/components/CVItemLink.tsx
+++ cv_next/app/feed/components/CVItemLink.tsx
@@ -14,7 +14,7 @@ export default function CVItemLink({ cv, children }: CVCardProps) {
   return (
     <div
       id={cv.id}
-      className={`relative h-full w-full max-w-full cursor-pointer rounded-xl bg-white object-cover shadow-2xl`}
+      className="relative h-full w-full max-w-full cursor-pointer overflow-hidden rounded-xl bg-white object-cover shadow-2xl"
       onClick={() => router.push(`/cv/${encodedId}`)}
     >
       {children}
