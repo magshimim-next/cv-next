2e12a6883efe3ff735bba342c3d4be9274def46f
Arad Donenfeld <aradon267@gmail.com>
2025-01-18 00:32:08 +0200

    ADDED: exclude array items

--- cv_next/app/feed/components/filters/valueSelect.tsx
+++ cv_next/app/feed/components/filters/valueSelect.tsx
@@ -10,6 +10,7 @@ export const DropdownInput = ({
   isApplyOptionVisible,
   noneText,
   selected,
+  exclude,
 }: {
   placeHolder: string;
   valueIds: number[];
@@ -20,6 +21,7 @@ export const DropdownInput = ({
   isApplyOptionVisible?: boolean;
   noneText: "all" | "none";
   selected?: number[] | null;
+  exclude?: number[];
 }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [selectedCategories, setSelectedCategories] = useState<number[]>(
@@ -94,24 +96,26 @@ export const DropdownInput = ({
           >
             {noneText}
           </div>
-          {valueIds.map((possibleValueId) => (
-            <label
-              key={possibleValueId}
-              className="flex h-8 w-full items-center justify-between bg-white text-black hover:bg-slate-200"
-              style={{ paddingLeft: "20px", paddingRight: "20px" }}
-              htmlFor={`checkbox-${possibleValueId}`}
-            >
-              <input
-                id={`checkbox-${possibleValueId}`}
-                type="checkbox"
-                checked={selectedCategories.includes(possibleValueId)}
-                onChange={() => {
-                  handleCheckboxChange(possibleValueId);
-                }}
-              />
-              {`${getValueById(possibleValueId)}`}
-            </label>
-          ))}
+          {valueIds
+            .filter((item) => !exclude?.includes(item))
+            .map((possibleValueId) => (
+              <label
+                key={possibleValueId}
+                className="flex h-8 w-full items-center justify-between bg-white text-black hover:bg-slate-200"
+                style={{ paddingLeft: "20px", paddingRight: "20px" }}
+                htmlFor={`checkbox-${possibleValueId}`}
+              >
+                <input
+                  id={`checkbox-${possibleValueId}`}
+                  type="checkbox"
+                  checked={selectedCategories.includes(possibleValueId)}
+                  onChange={() => {
+                    handleCheckboxChange(possibleValueId);
+                  }}
+                />
+                {`${getValueById(possibleValueId)}`}
+              </label>
+            ))}
           {isApplyOptionVisible && (
             <div
               className="flex h-10 w-full items-center justify-center bg-white text-black hover:bg-slate-200"
