c2fd0804c1446b7384e4a04ad21024de8ddaf8ca
Arad Donenfeld <aradon267@gmail.com>
2025-01-18 03:10:38 +0200

    UPDATED: using GetCurrentID

--- cv_next/app/cv/[cvId]/page.tsx
+++ cv_next/app/cv/[cvId]/page.tsx
@@ -5,14 +5,13 @@ import { getCvById } from "@/server/api/cvs";
 import { decodeValue } from "@/lib/utils";
 import { ScrollToTop } from "@/components/ui/scrollToTop";
 import { CvPreview } from "@/components/ui/cvPreview";
-import SupabaseHelper from "@/server/api/supabaseHelper";
+import { getCurrentId } from "@/server/api/users";
 import CommentsSection from "./components/commentSection/commentsSection";
 import CommentForm from "./components/commentSection/commentForm";
 import CvData from "./components/cvData";
 
 export default async function Page({ params }: { params: { cvId: string } }) {
   const { cvId } = params;
-  const supabase = SupabaseHelper.getSupabaseInstance();
   const decodedCvId = decodeValue(decodeURIComponent(cvId));
   if (!decodedCvId) {
     notFound();
@@ -28,9 +27,9 @@ export default async function Page({ params }: { params: { cvId: string } }) {
   });
   const validCV = resp.status === 200;
   const fullCV = validCV ? "md:grid-cols-[70%_30%]" : "";
-  const userId = await supabase.auth.getUser();
-  if (userId.error || !userId.data.user) {
-    return notFound();
+  const userId = await getCurrentId();
+  if (!userId.ok) {
+    notFound();
   }
   const authorData = JSON.parse(JSON.stringify(cv.user_id));
 
@@ -40,7 +39,7 @@ export default async function Page({ params }: { params: { cvId: string } }) {
       <CvData
         cv={cv}
         validCV={validCV}
-        currentUserIsAuthor={userId.data.user.id == authorData.id}
+        currentUserIsAuthor={userId.val == authorData.id}
       />
       <div className={`grid grid-cols-1 gap-y-4 ${fullCV} md:gap-x-4`}>
         <section className=" flex-col rounded-lg">
