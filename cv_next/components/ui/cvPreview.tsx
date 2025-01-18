3c7661c523cbd35d841f1706fc1e08fa4dffa19a
Arad Donenfeld <aradon267@gmail.com>
2025-01-18 02:45:36 +0200

    ADDED: optional height

--- cv_next/components/ui/cvPreview.tsx
+++ cv_next/components/ui/cvPreview.tsx
@@ -1,11 +1,15 @@
 import { transformToPreviewLink } from "@/lib/utils";
 
-export const CvPreview = ({ cv }: { cv: CvModel }) => {
+export const CvPreview = ({ cv, height }: { cv: CvModel; height?: number }) => {
   const previewUrl = transformToPreviewLink(cv.document_link);
 
   return (
     <div>
-      <iframe src={previewUrl + "&rm=minimal"} width="100%" height="800" />
+      <iframe
+        src={previewUrl + "&rm=minimal"}
+        width="100%"
+        height={height || "800"}
+      />
     </div>
   );
 };
