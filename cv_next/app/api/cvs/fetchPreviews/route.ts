1ba9faf1454b5335ad988999b0e80882a7d912c5
Arad Donenfeld <aradon267@gmail.com>
2025-01-26 00:10:42 +0200

    UPDATED: revalidate structure

--- cv_next/app/api/cvs/fetchPreviews/route.ts
+++ cv_next/app/api/cvs/fetchPreviews/route.ts
@@ -23,51 +23,64 @@ export async function POST(req: NextRequest) {
  * If the hash of the CV that was saved in the map is similar to the one that just got fetched
  * nothing is done. If something changed, the image is uploaded to supabase again
  * @param {string} cvLink - The cv link to validate
+ * @returns {Promise<NextResponse>} - The response with the public url of the image, or the relevant message
  */
-async function revalidatePreviewHandler(data: { cvLink: string }) {
+async function revalidatePreviewHandler(data: {
+  cvLink: string;
+}): Promise<NextResponse> {
   const cvLink = data.cvLink;
-  const id = getIdFromLink(cvLink);
-  const fileName = id + ".png";
-  const response = await fetch(getGoogleImageUrl(cvLink), {
+  const docsID = getIdFromLink(cvLink);
+  const fileName = docsID + ".png";
+  const fetchDocsResponse = await fetch(getGoogleImageUrl(cvLink), {
     next: { revalidate: Definitions.FETCH_WAIT_TIME },
     redirect: "manual",
   });
 
-  if (response.status === 302) {
+  if (fetchDocsResponse.status === 302) {
     logger.error("Redirected when asked for usercontent, probably private");
     return NextResponse.json({ message: "CV is private" }, { status: 500 });
   }
 
-  const blob = await response.blob();
+  const docsBlob = await fetchDocsResponse.blob();
   const isSimilar = await compareHashes(
-    blob,
-    blobDataMap.get(id || "") || new Blob()
+    docsBlob,
+    blobDataMap.get(docsID || "") || new Blob()
   );
-  if (!isSimilar) {
-    blobDataMap.delete(id || "");
-    blobDataMap.set(id || "", blob);
 
-    const { data, error } = await SupabaseHelper.getSupabaseInstance()
+  if (isSimilar) {
+    logger.debug("Files were similar");
+    return NextResponse.json({ message: "No changes" });
+  }
+
+  blobDataMap.delete(docsID || "");
+  blobDataMap.set(docsID || "", docsBlob);
+
+  const { data: uploadedData, error: uploadError } =
+    await SupabaseHelper.getSupabaseInstance()
       .storage.from(Storage.cvs)
-      .upload(fileName, blob, {
+      .upload(fileName, docsBlob, {
         cacheControl: "3600",
         upsert: true,
       });
 
-    if (error && !error?.message.includes("violates row-level security")) {
-      logger.error(error, "Upload error:");
-    } else if (error) {
-      // debug because it's redundant to log RLS
-      logger.debug(error, "RLS error on upload");
-    } else {
-      logger.debug(data, "File uploaded successfully:");
-      const publicUrl = SupabaseHelper.getSupabaseInstance()
-        .storage.from(Storage.cvs)
-        .getPublicUrl(fileName).data.publicUrl;
-      return NextResponse.json({ publicUrl });
-    }
+  if (
+    uploadError &&
+    !uploadError?.message.includes("violates row-level security")
+  ) {
+    logger.error(uploadError, "Upload error:");
+    return NextResponse.json(
+      { message: "Error revalidating CV" },
+      { status: 500 }
+    );
+  } else if (uploadError) {
+    // debug because it's redundant to log RLS
+    logger.debug(uploadError, "RLS error on upload");
+    return NextResponse.json({ message: "RLS error" });
   } else {
-    logger.debug("Files were similar");
+    logger.debug(uploadedData, "File uploaded successfully:");
+    const publicUrl = SupabaseHelper.getSupabaseInstance()
+      .storage.from(Storage.cvs)
+      .getPublicUrl(fileName).data.publicUrl;
+    return NextResponse.json({ publicUrl });
   }
-  return NextResponse.json({ message: "No changes" });
 }
