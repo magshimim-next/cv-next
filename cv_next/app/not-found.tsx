8acea271679b4f89bababc0a8dbc8a11a8d65c81
Arad Donenfeld <aradon267@gmail.com>
2025-01-25 02:35:30 +0200

    FIXED: not found on mobile

--- cv_next/app/not-found.tsx
+++ cv_next/app/not-found.tsx
@@ -1,53 +1,36 @@
 import Image from "next/image";
-import notFound404 from "@/public/images/404.png";
-import notFound404Light from "@/public/images/404Light.png";
 import blackChair from "@/public/images/chair.png";
 import whiteChair from "@/public/images/whiteChair.png";
 
 export default function NotFound() {
   return (
-    <div>
-      <div className="left-0 flex h-3/4 w-full items-center justify-center space-y-8 p-6">
-        <div className="flex flex-row items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <div className="items-center-justify-center flex flex-row">
-              <div>
-                <Image
-                  alt="closeIcon"
-                  src={notFound404}
-                  className="dark:hidden"
-                ></Image>
-                <Image
-                  alt="closeIcon"
-                  src={notFound404Light}
-                  className="hidden dark:block"
-                ></Image>
-              </div>
-              <h1 className="place-self-center pl-10 text-left text-7xl">
-                {" "}
-                Page Not Found
-              </h1>
-            </div>
-            <div className="items-center-justify-center flex flex-row">
-              <h2 className="place-self-center text-left text-3xl">
-                This page didn’t land the job. Try another page!&nbsp;
-                <a href="/" className="underline">
-                  Go Home
-                </a>
-              </h2>
-            </div>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center space-y-6">
+        <div className="flex flex-col items-center justify-center space-y-6 sm:mt-16 md:flex-row md:space-x-6 md:space-y-0">
+          <div className="mt-4 flex items-center justify-center ">
+            <Image
+              alt="blackChair"
+              src={blackChair}
+              className="w-40 dark:hidden sm:w-56"
+            />
+            <Image
+              alt="whiteChair"
+              src={whiteChair}
+              className="hidden w-40 dark:block sm:w-56"
+            />
           </div>
-          <Image
-            alt="blackChair"
-            src={blackChair}
-            className="w-1/2 dark:hidden"
-          ></Image>
-          <Image
-            alt="whiteChair"
-            src={whiteChair}
-            className="hidden w-1/2 dark:block"
-          ></Image>
+
+          <h1 className="text-center text-4xl font-bold md:text-left md:text-7xl">
+            Page Not Found
+          </h1>
         </div>
+
+        <h2 className="text-center text-xl md:text-left md:text-3xl">
+          This page didn’t land the job. Try another page!&nbsp;
+          <a href="/" className="text-blue-600 underline hover:text-blue-800">
+            Go Home
+          </a>
+        </h2>
       </div>
     </div>
   );
