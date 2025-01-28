93e97f4c4d3baa072e0e7c8425b93caa7beb53c7
Arad Donenfeld <aradon267@gmail.com>
2025-01-28 11:47:11 +0200

    UPDATED: shadow and mobile

--- cv_next/components/layout/pageFooter.tsx
+++ cv_next/components/layout/pageFooter.tsx
@@ -62,17 +62,19 @@ const Footer: React.FC = () => {
           &copy; {new Date().getFullYear()} Magshimim Next. All rights reserved.
         </p>
       </div>
-      <div className="flex justify-center">
-        <div className="flex h-fit max-w-fit items-center justify-center rounded-md px-5 text-center text-xs opacity-50 shadow-md hover:shadow-xl sm:text-left">
-          {`Credit to our team: ${Credits.slice(0, -1).join(", ")} and ${Credits.slice(-1)}`}
+      <div className="mt-1 shadow-md">
+        <div className="mb-2 flex justify-center">
+          <div className="mb-2 flex h-fit max-w-fit items-center justify-center rounded-md px-5 text-center text-xs opacity-50 hover:shadow-xl sm:text-left">
+            {`Credit to our team: ${Credits.slice(0, -1).join(", ")} and ${Credits.slice(-1)}`}
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex h-fit max-w-fit items-center justify-center rounded-md px-5 text-center text-xs opacity-50 shadow-md hover:shadow-xl sm:text-left">
-          {`Want to see more amazing people? Go to our `}
-          <Link href="/hall" className="text-blue-500 hover:underline">
-            &nbsp;Hall of Fame
-          </Link>
+        <div className="flex justify-center rounded-md text-center text-xs opacity-50 hover:shadow-xl sm:text-left">
+          <span className="mb-2 text-xs opacity-75">
+            {`Want to see more amazing people? Go to our `}
+            <Link href="/your-page" className="text-blue-500 hover:underline">
+              &nbsp;Hall of Fame
+            </Link>
+          </span>
         </div>
       </div>
     </footer>
