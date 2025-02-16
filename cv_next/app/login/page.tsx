9c422e05ec1d5cd8228edd0bf3d10739cf458d05
Arad Donenfeld <aradon267@gmail.com>
2025-02-16 15:24:20 +0200

    ADDED: titles to each client side page

--- cv_next/app/login/page.tsx
+++ cv_next/app/login/page.tsx
@@ -5,6 +5,7 @@ import { LoginLayout } from "./components/loginLayout";
 export default function Page() {
   return (
     <main>
+      <title>Login</title>
       <Suspense fallback={<div>Loading...</div>}>
         <LoginLayout />
       </Suspense>
