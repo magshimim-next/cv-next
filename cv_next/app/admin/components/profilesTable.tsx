baf5e621e120ee59e5133053b2acb65fc2fa1afd
Arad Donenfeld <aradon267@gmail.com>
2025-02-22 15:05:50 +0200

    ADDED: profiles table file | UPDATED: using scroll to top

---
+++ cv_next/app/admin/components/profilesTable.tsx
@@ -0,0 +1,62 @@
+"use server";
+import Image from "next/image";
+import { PermsDropDown } from "./permsDropDown";
+
+export const ProfilesTable = ({
+  profiles,
+}: {
+  profiles: Partial<UserWithPerms>[];
+}) => {
+  return (
+    <div className="w-full rounded-lg shadow-md">
+      <table className="mx-auto w-full max-w-full rounded-lg border border-gray-300 bg-white text-center dark:border-gray-700 dark:bg-black">
+        <thead className="bg-gray-100 dark:bg-gray-900">
+          <tr className=" text-sm uppercase text-gray-700 dark:text-gray-300">
+            <th className="whitespace-nowrap border p-4">ID</th>
+            <th className="whitespace-nowrap border p-4">Display Name</th>
+            <th className="whitespace-nowrap border p-4">Username</th>
+            <th className="whitespace-nowrap border p-4">Avatar</th>
+            <th className="whitespace-nowrap border p-4">User Type</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {profiles.map((profile) => (
+            <tr key={profile.id}>
+              <td className="whitespace-nowrap border p-4 text-gray-700 dark:text-gray-300">
+                {profile.id}
+              </td>
+              <td className="whitespace-nowrap border p-4 font-medium">
+                {profile.display_name}
+              </td>
+              <td className="whitespace-nowrap border p-4 ">
+                {profile.username}
+              </td>
+              {profile.avatar_url ? (
+                <td className="border p-4 text-center">
+                  <div className="relative mx-auto h-12 w-12">
+                    <Image
+                      src={profile.avatar_url}
+                      alt={profile.display_name || ""}
+                      width={125}
+                      height={75}
+                      className="h-full w-full rounded-full border object-contain"
+                    />
+                  </div>
+                </td>
+              ) : (
+                <td />
+              )}
+              <td className="whitespace-nowrap border p-4">
+                <PermsDropDown
+                  userId={profile.id || ""}
+                  currentPerms={profile.user_type || ""}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- cv_next/app/admin/page.tsx
+++ cv_next/app/admin/page.tsx
@@ -1,8 +1,8 @@
 "use server";
-import Image from "next/image";
 import { Metadata } from "next";
 import { getAllUsers } from "@/server/api/users";
-import { PermsDropDown } from "./components/permsDropDown";
+import { ScrollToTop } from "@/components/ui/scrollToTop";
+import { ProfilesTable } from "./components/profilesTable";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
@@ -12,65 +12,15 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Page() {
   const users = await getAllUsers();
-  if (users.ok) {
-    return (
+
+  return (
+    <div>
+      <ScrollToTop />
       <div className="mx-auto mt-10 ">
-        <h1 className="mb-6 text-center text-2xl font-semibold">
-          Admin Panel - User Approvals
-        </h1>
+        <h1 className="mb-6 text-center text-2xl font-semibold">Admin Panel</h1>
 
-        <div className="w-full rounded-lg shadow-md">
-          <table className="mx-auto w-full max-w-full rounded-lg border border-gray-300 bg-white text-center dark:border-gray-700 dark:bg-black">
-            <thead className="bg-gray-100 dark:bg-gray-900">
-              <tr className=" text-sm uppercase text-gray-700 dark:text-gray-300">
-                <th className="whitespace-nowrap border p-4">ID</th>
-                <th className="whitespace-nowrap border p-4">Display Name</th>
-                <th className="whitespace-nowrap border p-4">Username</th>
-                <th className="whitespace-nowrap border p-4">Avatar</th>
-                <th className="whitespace-nowrap border p-4">User Type</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {users.val.map((profile) => (
-                <tr key={profile.id}>
-                  <td className="whitespace-nowrap border p-4 text-gray-700 dark:text-gray-300">
-                    {profile.id}
-                  </td>
-                  <td className="whitespace-nowrap border p-4 font-medium">
-                    {profile.display_name}
-                  </td>
-                  <td className="whitespace-nowrap border p-4 ">
-                    {profile.username}
-                  </td>
-                  {profile.avatar_url ? (
-                    <td className="border p-4 text-center">
-                      <div className="relative mx-auto h-12 w-12">
-                        <Image
-                          src={profile.avatar_url}
-                          alt={profile.display_name || ""}
-                          width={125}
-                          height={75}
-                          className="h-full w-full rounded-full border object-contain"
-                        />
-                      </div>
-                    </td>
-                  ) : (
-                    <td />
-                  )}
-                  <td className="whitespace-nowrap border p-4">
-                    <PermsDropDown
-                      userId={profile.id || ""}
-                      currentPerms={profile.user_type || ""}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        {users.ok && <ProfilesTable profiles={users.val} />}
       </div>
-    );
-  }
-  return <div>Error: {users.errors as string}</div>;
+    </div>
+  );
 }
