d9e4fd7ef7d65e110119413cba05d7c2562c8c0d
Arad Donenfeld <aradon267@gmail.com>
2025-02-22 14:40:06 +0200

    ADDED: error messages for updateing admin

--- cv_next/app/admin/components/permsDropDown.tsx
+++ cv_next/app/admin/components/permsDropDown.tsx
@@ -2,6 +2,8 @@
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
 import { PermsKeys } from "@/lib/supabase-definitions";
+import { useError } from "@/providers/error-provider";
+import { Visible_Error_Messages } from "@/lib/definitions";
 
 type FormValues = {
   perms: keyof typeof PermsKeys.user_types_enum;
@@ -16,8 +18,16 @@ export const PermsDropDown = ({
 }) => {
   const [newPerms, setNewPerms] = useState<string>(currentPerms);
   const { handleSubmit, register } = useForm<FormValues>({ mode: "onChange" });
+  const { showError } = useError();
 
   const handleOnSubmit: SubmitHandler<FormValues> = async (data) => {
+    if (currentPerms === PermsKeys.user_types_enum.admin) {
+      showError(
+        Visible_Error_Messages.UpdateAdminPerms.title,
+        Visible_Error_Messages.UpdateAdminPerms.description
+      );
+      return;
+    }
     setNewPerms(data.perms);
     console.log(data);
   };
