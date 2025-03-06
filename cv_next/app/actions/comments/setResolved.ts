ca6aa8b141dfd29c9eef14ef51ce8ba3762ebbad
Arad Donenfeld <aradon267@gmail.com>
2025-03-06 23:51:49 +0200

    UPDATED: admins can resolve others comments

--- cv_next/app/actions/comments/setResolved.ts
+++ cv_next/app/actions/comments/setResolved.ts
@@ -5,8 +5,45 @@ import {
   getCommentById,
   setResolved as setCommentResolved,
 } from "@/server/api/comments";
-import { getCurrentId } from "@/server/api/users";
+import { getCurrentId, userIsAdmin } from "@/server/api/users";
 import logger, { logErrorWithTrace } from "@/server/base/logger";
+
+/**
+ * The function will see if the comment can be resolved
+ * @param commentResult The comment model to be resolved
+ * @returns A promise of error or null based on if the action can be performed
+ */
+async function validateResolve(
+  commentResult: Result<CommentModel, string>
+): Promise<Result<never, string> | null> {
+  const currentIdResult = await getCurrentId();
+  if (!currentIdResult.ok) {
+    logErrorWithTrace(currentIdResult);
+    return Err(
+      "An error has occurred while resolving the comment. Please try again later."
+    );
+  }
+
+  if (!commentResult.ok) {
+    logErrorWithTrace(commentResult);
+    return Err(
+      "An error has occurred while resolving the comment. Please try again later."
+    );
+  }
+  const resultAdminCheck = await userIsAdmin();
+  if (
+    currentIdResult.val != commentResult.val.user_id &&
+    !resultAdminCheck.ok
+  ) {
+    logger.error(
+      `${currentIdResult.val} tried resolving someone elses comment!`
+    );
+    return Err(
+      "An error has occurred while resolving the comment. Please try again later."
+    );
+  }
+  return null;
+}
 
 /**
  * Sets a comment as resolved or unresolved.
@@ -34,14 +71,11 @@ export const setResolved = async (
     );
   }
 
-  if (currentIdResult.val != commentResult.val.user_id) {
-    logger.error(
-      `${currentIdResult.val} tried resolving someone elses comment!`
-    );
-    return Err(
-      "An error has occurred while resolving the comment. Please try again later."
-    );
+  const validateError = await validateResolve(commentResult);
+  if (validateError) {
+    return validateError;
   }
+
   const result = await setCommentResolved(commentId, resolved);
   if (result.ok) {
     return result;
