b54363454855e4d84ef93a97b5e5c29e35785f3b
Arad Donenfeld <aradon267@gmail.com>
2025-06-01 20:02:04 +0300

    FIXED: using custom tooltip instead of default tooltip

--- cv_next/app/cv/[cvId]/components/downloadButtons.tsx
+++ cv_next/app/cv/[cvId]/components/downloadButtons.tsx
@@ -2,6 +2,7 @@ import { PiFilePdfDuotone } from "react-icons/pi";
 import { TbFileTypeDocx } from "react-icons/tb";
 import { HiOutlineSave } from "react-icons/hi";
 import { getExportLinks } from "@/helpers/imageURLHelper";
+import Tooltip from "@/components/ui/tooltip";
 
 export default function DownloadButtons({ cvLink }: { cvLink: string }) {
   const exportLinks = getExportLinks(cvLink);
@@ -14,35 +15,38 @@ export default function DownloadButtons({ cvLink }: { cvLink: string }) {
   return (
     <div className="flex gap-2">
       {isSameLink ? (
-        <a
-          href={pdfUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center gap-1 rounded-md px-3 py-1.5 text-sm"
-          title="Download"
-        >
-          <HiOutlineSave size={25} />
-        </a>
-      ) : (
-        <>
+        <Tooltip id="DownloadFromDrive" message="Download">
           <a
             href={pdfUrl}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-1 rounded-md px-3 py-1.5 text-sm"
-            title="Download PDF"
           >
-            <PiFilePdfDuotone size={25} />
+            <HiOutlineSave size={25} />
           </a>
-          <a
-            href={docxUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-1 rounded-md px-3 py-1.5 text-sm"
-            title="Download DOCX"
-          >
-            <TbFileTypeDocx size={22} />
-          </a>
+        </Tooltip>
+      ) : (
+        <>
+          <Tooltip id="DownloadAsPDF" message="Download PDF">
+            <a
+              href={pdfUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 rounded-md px-3 py-1.5 text-sm"
+            >
+              <PiFilePdfDuotone size={25} />
+            </a>
+          </Tooltip>
+          <Tooltip id="DownloadAsDOCX" message="Download DOCX">
+            <a
+              href={docxUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-1 rounded-md px-3 py-1.5 text-sm"
+            >
+              <TbFileTypeDocx size={22} />
+            </a>
+          </Tooltip>
         </>
       )}
     </div>
