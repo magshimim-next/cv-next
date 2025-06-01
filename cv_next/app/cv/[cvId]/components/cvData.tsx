ccf0e4b05e4dabad264a058a414ca2cd103546bc
Arad Donenfeld <aradon267@gmail.com>
2025-06-01 19:57:27 +0300

    UPDATED: cvData includes download buttons

--- cv_next/app/cv/[cvId]/components/cvData.tsx
+++ cv_next/app/cv/[cvId]/components/cvData.tsx
@@ -7,6 +7,7 @@ import DynamicProfileImage from "@/components/ui/DynamicProfileImage";
 import { useError } from "@/providers/error-provider";
 import { Visible_Error_Messages } from "@/lib/definitions";
 import { CvCategory } from "@/components/ui/cvCategory";
+import DownloadButtons from "./downloadButtons";
 
 export default function CvData({
   cv,
@@ -43,23 +44,28 @@ export default function CvData({
   return (
     <div className={`grid grid-cols-1 gap-y-4 ${showData} md:gap-x-4`}>
       <article className="mb-3 flex flex-col rounded-lg border-b border-gray-200 bg-white p-6 text-base dark:bg-theme-800">
-        <div className="mb-3 flex items-center">
-          <div className="mr-3">
-            <DynamicProfileImage
-              isPlaceholder={uploader.avatar_url ? false : true}
-              placeHolderStyle={{ fontSize: "35px" }}
-            >
-              <Image
-                alt="profile"
-                src={uploader.avatar_url || ""}
-                width={40}
-                height={30 * 1.4142}
-              />
-            </DynamicProfileImage>
+        <div className="mb-3 flex items-center justify-between">
+          <div className="flex items-center">
+            <div className="mr-3">
+              <DynamicProfileImage
+                isPlaceholder={!uploader.avatar_url}
+                placeHolderStyle={{ fontSize: "35px" }}
+              >
+                <Image
+                  alt="profile"
+                  src={uploader.avatar_url || ""}
+                  width={40}
+                  height={30 * 1.4142}
+                />
+              </DynamicProfileImage>
+            </div>
+            <Link href={`/profile/${uploader.username}`}>
+              <p className="text-xl font-medium hover:underline">
+                {displayName}
+              </p>
+            </Link>
           </div>
-          <Link href={`/profile/${uploader.username}`}>
-            <p className="text-xl font-medium hover:underline">{displayName}</p>
-          </Link>
+          <DownloadButtons cvLink={cv.document_link} />
         </div>
         <div className="mb-3 flex flex-wrap items-center space-x-2">
           {cv.cv_categories && (
