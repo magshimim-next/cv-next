6b4a25868ef0eb1c4570f9cbc465d82c7bd076c4
Arad Donenfeld <aradon267@gmail.com>
2025-06-13 20:14:38 +0300

    UPDATED: buttons support type and classname

--- cv_next/app/feed/components/button.tsx
+++ cv_next/app/feed/components/button.tsx
@@ -5,16 +5,21 @@ export const Button = ({
   onClick,
   bottomFlat,
   isDisabled,
+  className,
+  type,
 }: {
   bottomFlat?: boolean;
   text: ReactNode;
   onClick: () => void;
   isDisabled?: boolean;
+  className?: string;
+  type?: "button" | "submit" | "reset";
 }) => {
   const menuBorderStyle = bottomFlat ? "outline-b-0 rounded-b-none" : "";
   return (
     <button
-      className={`outline-gray-40 flex h-full w-full items-center justify-center whitespace-nowrap bg-white text-black ${isDisabled ? "opacity-25" : "cursor-pointer hover:bg-slate-200"} outline-2 ${menuBorderStyle} box-border rounded-md px-10 py-4`}
+      type={type || "submit"}
+      className={`${className} outline-gray-40 flex h-full w-full items-center justify-center whitespace-nowrap bg-white text-black ${isDisabled ? "opacity-25" : "cursor-pointer hover:bg-slate-200"} outline-2 ${menuBorderStyle} box-border rounded-md px-10 py-4`}
       onClick={onClick}
       disabled={isDisabled}
     >
