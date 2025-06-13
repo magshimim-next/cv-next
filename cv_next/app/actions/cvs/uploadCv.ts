6f059ec3bc552c91886c59902eb53fd98a424400
Arad Donenfeld <aradon267@gmail.com>
2025-06-13 18:55:41 +0300

    ADDED: updateCv server action

--- cv_next/app/actions/cvs/uploadCv.ts
+++ cv_next/app/actions/cvs/uploadCv.ts
@@ -1,11 +1,12 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { uploadCV, getCvsByUserId } from "@/server/api/cvs";
-import { encodeValue } from "@/lib/utils";
+import { uploadCV, getCvsByUserId, updateCV } from "@/server/api/cvs";
+import { encodeValue, Err, Ok } from "@/lib/utils";
 import { transformGoogleViewOnlyUrl } from "@/helpers/cvLinkRegexHelper";
-import logger from "@/server/base/logger";
+import logger, { logErrorWithTrace } from "@/server/base/logger";
 import SupabaseHelper from "@/server/api/supabaseHelper";
+import { getCurrentId, userIsAdmin } from "@/server/api/users";
 export interface InputValues {
   link: string;
   description: string;
@@ -67,7 +68,7 @@ export const checkUploadCV = async ({
     cv_categories: cvData.cvCategories,
   };
 
-  logger.debug("Can upload:", cvToUpload);
+  logger.debug(cvToUpload, "Can upload:");
 
   const response = await uploadCV(cvToUpload);
   if (response) {
@@ -82,3 +83,90 @@ export const canUserUploadACV = async (userId: string) => {
   const cvsOfUser = await getCvsByUserId(userId);
   return !cvsOfUser || cvsOfUser.length < 5;
 };
+
+/**
+ * The function will update a CV if the user is allowed to do so.
+ * @param {CvModel} cvData The CV data to be updated
+ * @returns {Promise<string | null>} A promise that resolves to an error message if the update fails, or null if it succeeds
+ */
+export const checkUpdateCV = async ({
+  cvData,
+}: {
+  cvData: CvModel;
+}): Promise<string | null> => {
+  // TODO: change to the getUser of #99 after merge
+  const authorId = cvData.user_id as any;
+  cvData.user_id = authorId["id"];
+
+  const validateError = await validateUpdate(cvData);
+  if (!validateError.ok) {
+    logger.error(validateError, "Validation error");
+    return (
+      validateError.errors.err?.message ||
+      "Failed to update CV. Please try again later."
+    );
+  }
+  const transformedURL = transformGoogleViewOnlyUrl(cvData.document_link);
+
+  if (transformedURL == "") {
+    logger.error("Couldn't transform the link %s", cvData.document_link);
+    return "Regex invalid!";
+  }
+
+  const res = await fetch(cvData.document_link, {
+    method: "HEAD",
+  });
+
+  if (res.status !== 200) {
+    if (res.status === 302) {
+      logger.error("Redirected when asked for usercontent, probably private");
+      return "CV File is Private";
+    }
+    logger.error("Couldn't Find The CV %s", cvData.document_link);
+    return "Invalid URL for CV";
+  }
+
+  logger.debug(cvData, "Can updaate:");
+
+  const response = await updateCV(cvData);
+  if (response) {
+    redirect(`/cv/${encodeValue(cvData.id)}`);
+  } else {
+    logger.error(response, "Error updating CV");
+    return "Error updating CV";
+  }
+};
+
+/**
+ * The function will see if the comment can be deleted
+ * @param commentResult The comment model to be deleted
+ * @returns A promise of error or null based on if the action can be performed
+ */
+async function validateUpdate(cvData: CvModel): Promise<Result<void, string>> {
+  const currentIdResult = await getCurrentId();
+  if (!currentIdResult.ok) {
+    logErrorWithTrace(currentIdResult);
+    return Err(
+      "An error has occurred while deleting the comment. Please try again later."
+    );
+  }
+
+  if (
+    !cvData.document_link?.trim() ||
+    !cvData.description?.trim() ||
+    cvData.cv_categories == undefined ||
+    cvData.cv_categories.length <= 0
+  ) {
+    logger.error("Missing variables!");
+    return Err("Some variables are missing! Please fill them in.");
+  }
+
+  const resultAdminCheck = await userIsAdmin();
+  if (currentIdResult.val != cvData.user_id && !resultAdminCheck.ok) {
+    logger.error(`${currentIdResult.val} tried updating someone elses CV!`);
+    return Err(
+      "An error has occurred while updating the CV. Please try again later."
+    );
+  }
+  return Ok.EMPTY;
+}
