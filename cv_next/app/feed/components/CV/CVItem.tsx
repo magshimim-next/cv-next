86734d9aec1fa44160559d7914de911a6edbf093
Arad Donenfeld <aradon267@gmail.com>
2025-08-26 00:13:06 +0300

    UPDATED: CVItem and CvLink merged to close hydration errors

--- cv_next/app/feed/components/CV/CVItem.tsx
+++ cv_next/app/feed/components/CV/CVItem.tsx
@@ -2,8 +2,9 @@
 
 import Image from "next/image";
 import { useState, useEffect, useRef, useCallback } from "react";
+import Link from "next/link";
 import Definitions, { API_DEFINITIONS } from "@/lib/definitions";
-import { shimmer, toBase64 } from "@/lib/utils";
+import { encodeValue, shimmer, toBase64 } from "@/lib/utils";
 import { useApiFetch } from "@/hooks/useAPIFetch";
 import access_denied from "@/public/images/access_denied.png";
 import CategoriesDisplay from "./categoryDisplay";
@@ -73,34 +74,38 @@ export default function CVItem({ cv }: CVCardProps) {
   const formattedDate = new Date(cv.created_at).toLocaleDateString("en-US");
 
   return (
-    <div className="flex h-full flex-col overflow-hidden rounded-lg bg-white shadow">
-      <div className="aspect-[1/1.414] w-full overflow-hidden rounded-lg bg-gray-100">
-        <Image
-          src={
-            realURL.length > 1
-              ? realURL
-              : `data:image/svg+xml;base64,${toBase64(shimmer(300, 300))}`
-          }
-          width={500}
-          height={500 * 1.4142}
-          alt="CV Preview"
-          className="h-full w-full object-contain object-top p-2"
-          placeholder="blur"
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(300, 300))}`}
-          priority
-        />
-      </div>
+    <div className="relative h-full w-full max-w-full overflow-hidden rounded-xl bg-white shadow-2xl">
+      <div className="flex h-full flex-col overflow-hidden rounded-lg bg-white shadow">
+        <Link id={cv.id} href={`/cv/${encodeValue(cv.id)}`} scroll={false}>
+          <div className="aspect-[1/1.414] w-full overflow-hidden rounded-lg bg-gray-100">
+            <Image
+              src={
+                realURL.length > 1
+                  ? realURL
+                  : `data:image/svg+xml;base64,${toBase64(shimmer(300, 300))}`
+              }
+              width={500}
+              height={500 * 1.4142}
+              alt="CV Preview"
+              className="h-full w-full object-contain object-top p-2"
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(300, 300))}`}
+              priority
+            />
+          </div>
+        </Link>
 
-      <div className="overlay gradient-blur-backdrop absolute bottom-0 flex h-full w-full rounded-lg backdrop-blur-[0.5px] transition hover:via-transparent hover:backdrop-blur-none">
-        <div className="overlay absolute bottom-0 h-1/5 w-full rounded-xl bg-transparent p-6">
-          <div className="absolute bottom-0 left-0 right-0 mx-5 mb-2.5">
-            <div className="flex flex-wrap items-baseline">
-              <div className="mr-2 text-xl font-bold text-neutral-700">
-                {authorObject.display_name}
+        <div className="overlay gradient-blur-backdrop pointer-events-none absolute bottom-0 flex h-full w-full rounded-lg backdrop-blur-[0.5px] transition hover:via-transparent hover:backdrop-blur-none">
+          <div className="overlay pointer-events-auto absolute bottom-0 h-1/5 w-full rounded-xl bg-transparent p-6">
+            <div className="absolute bottom-0 left-0 right-0 mx-5 mb-2.5">
+              <div className="flex flex-wrap items-baseline">
+                <div className="mr-2 text-xl font-bold text-neutral-700">
+                  {authorObject.display_name}
+                </div>
+                <p className="text-xs text-neutral-400">{formattedDate}</p>
               </div>
-              <p className="text-xs text-neutral-400">{formattedDate}</p>
+              <CategoriesDisplay categories={cv.cv_categories} />
             </div>
-            <CategoriesDisplay categories={cv.cv_categories} />
           </div>
         </div>
       </div>
