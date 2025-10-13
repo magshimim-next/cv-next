5ab3ce59a653c6a6421016344cb6d76167d34553
Arad Donenfeld <aradon267@gmail.com>
2025-10-13 15:09:10 +0300

    ADDED: general clickable component

--- cv_next/app/cv/[cvId]/page.tsx
+++ cv_next/app/cv/[cvId]/page.tsx
@@ -2,14 +2,13 @@
 
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
-import Link from "next/link";
 import { getCvById, getRandomizedCvs } from "@/server/api/cvs";
-import { decodeValue, encodeValue } from "@/lib/utils";
+import { decodeValue } from "@/lib/utils";
 import { ScrollToTop } from "@/components/ui/scrollToTop";
 import { CvPreview } from "@/components/ui/cvPreview";
 import { getCurrentId, userIsAdmin } from "@/server/api/users";
-import CategoriesDisplay from "@/app/feed/components/CV/categoryDisplay";
 import CarouselStructure from "@/components/ui/Carousel/carouselStructure";
+import { CVClickableCard } from "@/components/ui/CVComponents/cvClickableCard";
 import CommentsSection from "./components/commentSection/commentsSection";
 import CommentForm from "./components/commentSection/commentForm";
 import CvData from "./components/cvData";
@@ -121,34 +120,7 @@ export default async function Page({ params }: { params: { cvId: string } }) {
           <CarouselStructure options={{ loop: true }}>
             {recommendedCVs.map((recCv: CvModel) => (
               <div className="p-4" key={recCv.id}>
-                <Link
-                  href={`/cv/${encodeValue(recCv.id)}`}
-                  className="block h-full no-underline hover:no-underline"
-                >
-                  <div className="flex h-72 min-w-[400px] max-w-md cursor-pointer flex-col items-center justify-center gap-4 overflow-hidden rounded-2xl border-gray-200 bg-white py-6 shadow-lg transition hover:shadow-xl dark:border-gray-700 dark:bg-gray-800">
-                    <div className="flex w-full flex-shrink-0 flex-col items-center justify-center gap-1">
-                      <div className="w-full text-center text-2xl font-bold">
-                        {typeof recCv.user_id === "object" &&
-                        recCv.user_id !== null
-                          ? (recCv.user_id as { display_name?: string })
-                              .display_name
-                          : String(recCv.user_id)}
-                      </div>
-                      <p className="w-full text-center text-base text-neutral-400">
-                        {new Date(recCv.created_at).toLocaleDateString("en-US")}
-                      </p>
-                    </div>
-                    <div className="flex w-full flex-shrink-0 justify-center">
-                      <CategoriesDisplay categories={recCv.cv_categories} />
-                    </div>
-                    <div
-                      className="line-clamp-4 w-full flex-grow-0 px-2 text-center text-lg text-neutral-700 dark:text-white"
-                      style={{ height: "4.5em", overflow: "hidden" }}
-                    >
-                      {recCv.description}
-                    </div>
-                  </div>
-                </Link>
+                <CVClickableCard cv={recCv} />
               </div>
             ))}
           </CarouselStructure>
---
+++ cv_next/components/ui/CVComponents/cvClickableCard.tsx
@@ -0,0 +1,42 @@
+"use server";
+
+import Link from "next/link";
+import CategoriesDisplay from "@/app/feed/components/CV/categoryDisplay";
+import { encodeValue } from "@/lib/utils";
+
+/**
+ * CV card clickable component the show some key things about a given CV.
+ * Used in the recommendations carousels.
+ * @param {CvModel} param0 The CV to display.
+ * @returns {JSX.Element} The clickable card component.
+ */
+export async function CVClickableCard({ cv }: { cv: CvModel }) {
+  return (
+    <Link
+      href={`/cv/${encodeValue(cv.id)}`}
+      className="block h-full no-underline hover:no-underline"
+    >
+      <div className="flex h-72 min-w-[400px] max-w-md cursor-pointer flex-col items-center justify-center gap-4 overflow-hidden rounded-2xl border-gray-200 bg-white py-6 shadow-lg transition hover:shadow-xl dark:border-gray-700 dark:bg-gray-800">
+        <div className="flex w-full flex-shrink-0 flex-col items-center justify-center gap-1">
+          <div className="w-full text-center text-2xl font-bold">
+            {typeof cv.user_id === "object" && cv.user_id !== null
+              ? (cv.user_id as { display_name?: string }).display_name
+              : String(cv.user_id)}
+          </div>
+          <p className="w-full text-center text-base text-neutral-400">
+            {new Date(cv.created_at).toLocaleDateString("en-US")}
+          </p>
+        </div>
+        <div className="flex w-full flex-shrink-0 justify-center">
+          <CategoriesDisplay categories={cv.cv_categories} />
+        </div>
+        <div
+          className="line-clamp-4 w-full flex-grow-0 px-2 text-center text-lg text-neutral-700 dark:text-white"
+          style={{ height: "4.5em", overflow: "hidden" }}
+        >
+          {cv.description}
+        </div>
+      </div>
+    </Link>
+  );
+}
