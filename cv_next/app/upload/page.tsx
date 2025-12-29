f6d13fe33d09ebaad00efd4a0effc25f8301bd2d
Arad Donenfeld <aradon267@gmail.com>
2025-12-29 12:43:04 +0200

    UPDATED: upload form is in server component | ADDED: upload now has recommended Cvs

--- cv_next/app/upload/page.tsx
+++ cv_next/app/upload/page.tsx
@@ -1,88 +1,37 @@
-"use client";
+"use server";
 
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { Button } from "@/app/feed/components/button";
-import { useError } from "@/providers/error-provider";
-import { uploadCV } from "@/app/actions/cvs/uploadCv";
-import { LinkInput } from "@/components/ui/CVInput/LinkInput";
-import { CategoriesInput } from "@/components/ui/CVInput/CategoriesInput";
-import { DescriptionInput } from "@/components/ui/CVInput/DescriptionInput";
-import { ConfirmCheckbox } from "@/components/ui/ConfirmCheckbox";
-
-type FormValues = {
-  link: string;
-  description: string;
-  cvCategories: number[];
-};
+import { notFound } from "next/navigation";
+import { RecommendedCvsSection } from "@/components/ui/recommendedCvs";
+import Categories from "@/types/models/categories";
+import { getRandomEnumValues } from "@/lib/utils";
+import { getCurrentId } from "@/server/api/users";
+import UploadCVForm from "./components/uploadForm";
 
 /**
  * This page is used to upload a CV.
  * @returns {Element} The upload page for CVs.
  */
-export default function Page() {
-  const { showError } = useError();
-  const [isChecked, setIsChecked] = useState(false);
-  const checkboxMessage =
-    "I agree to my CV being publically available to community members.";
-
-  const {
-    control,
-    handleSubmit,
-    clearErrors,
-    setError,
-    formState: { errors, isValid },
-  } = useForm<FormValues>({
-    mode: "onChange",
-    defaultValues: {
-      link: "",
-      description: "",
-      cvCategories: [],
-    },
-  });
-
-  const onSubmit = async (data: FormValues) => {
-    const uploadResp = await uploadCV({
-      cvData: data,
-    });
-    if (uploadResp) {
-      showError(uploadResp, "");
-    }
-  };
+export default async function Page() {
+  const userId = await getCurrentId();
+  if (!userId.ok) {
+    notFound();
+  }
+  const randomCategories = getRandomEnumValues(Categories.category, 3);
 
   return (
-    <main className="flex flex-col items-center justify-center p-6 py-10">
-      <title>Upload CV</title>
-      <h1 className="mb-8 text-3xl font-bold">Upload CV</h1>
-      <form
-        className="flex w-full max-w-lg flex-col space-y-8"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <LinkInput control={control} errors={errors} />
-
-        <DescriptionInput
-          control={control}
-          errors={errors}
-          clearErrors={() => clearErrors}
-          setError={() => setError}
-        />
-
-        <CategoriesInput control={control} errors={errors} />
-
-        <ConfirmCheckbox
-          checked={isChecked}
-          onChange={setIsChecked}
-          message={checkboxMessage}
-        />
-
-        <div className="flex justify-center">
-          <Button
-            text="Submit"
-            onClick={handleSubmit(onSubmit)}
-            isDisabled={!isValid || !isChecked}
-          />
-        </div>
-      </form>
-    </main>
+    <div>
+      <div className="flex flex-col items-center justify-center py-6">
+        <title>Upload CV</title>
+        <h1 className="mb-5 text-3xl font-bold">Upload CV</h1>
+        <UploadCVForm />
+      </div>
+      <RecommendedCvsSection
+        filteredCategories={randomCategories}
+        currentUserId={userId.val}
+        amountToFetch={8}
+        title="Some Inspiration"
+        subtitle="Don't know where to begin? Have a look at some other CVs!"
+      />
+    </div>
   );
 }
