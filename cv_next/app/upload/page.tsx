b520d149e97639694d137cce5fc253851c1d9e88
Arad Donenfeld <aradon267@gmail.com>
2025-12-29 12:48:28 +0200

    UPDATED: texts in upload random CVs

--- cv_next/app/upload/page.tsx
+++ cv_next/app/upload/page.tsx
@@ -20,17 +20,21 @@ export default async function Page() {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center py-6">
+      <div
+        className="flex flex-col items-center justify-center"
+        style={{ paddingTop: "1.5rem" }}
+      >
         <title>Upload CV</title>
         <h1 className="mb-5 text-3xl font-bold">Upload CV</h1>
+        <h4>Need inspiration? Check below to see some CVs!</h4>
         <UploadCVForm />
       </div>
       <RecommendedCvsSection
         filteredCategories={randomCategories}
         currentUserId={userId.val}
         amountToFetch={8}
-        title="Some Inspiration"
-        subtitle="Don't know where to begin? Have a look at some other CVs!"
+        title="Looking for Inspiration?"
+        subtitle="Don't know where to begin? Have a look at some of the community member's CVs!"
       />
     </div>
   );
